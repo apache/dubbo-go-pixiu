6171850711ed3b35ed301f26a4168317ad3f2a72
william feng <>
2020-09-19 15:27:39 +0800

    included the integration config for api

--- pkg/config/api_config.go
+++ pkg/config/api_config.go
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package config
 
 import (
@@ -9,72 +26,6 @@ import (
 	"github.com/dubbogo/dubbo-go-proxy/pkg/common/yaml"
 	perrors "github.com/pkg/errors"
 )
-
-// import (
-// 	"sync"
-// )
-
-// var (
-// 	CacheApi = sync.Map{}
-// )
-
-// // Api is api gateway concept, control request from browser、Mobile APP、third party people
-// type Api struct {
-// 	Name     string      `json:"name"`
-// 	ITypeStr string      `json:"itype"`
-// 	IType    ApiType     `json:"-"`
-// 	OTypeStr string      `json:"otype"`
-// 	OType    ApiType     `json:"-"`
-// 	Status   Status      `json:"status"`
-// 	Metadata interface{} `json:"metadata"`
-// 	Method   string      `json:"method"`
-// 	RequestMethod
-// }
-
-// var EmptyApi = &Api{}
-
-// func NewApi() *Api {
-// 	return &Api{}
-// }
-
-// func (a *Api) FindApi(name string) (*Api, bool) {
-// 	if v, ok := CacheApi.Load(name); ok {
-// 		return v.(*Api), true
-// 	}
-
-// 	return nil, false
-// }
-
-// func (a *Api) MatchMethod(method string) bool {
-// 	i := RequestMethodValue[method]
-// 	if a.RequestMethod == RequestMethod(i) {
-// 		return true
-// 	}
-
-// 	return false
-// }
-
-// func (a *Api) IsOk(name string) bool {
-// 	if v, ok := CacheApi.Load(name); ok {
-// 		return v.(*Api).Status == Up
-// 	}
-
-// 	return false
-// }
-
-// // Offline api offline
-// func (a *Api) Offline(name string) {
-// 	if v, ok := CacheApi.Load(name); ok {
-// 		v.(*Api).Status = Down
-// 	}
-// }
-
-// // Online api online
-// func (a *Api) Online(name string) {
-// 	if v, ok := CacheApi.Load(name); ok {
-// 		v.(*Api).Status = Up
-// 	}
-// }
 
 // HTTPVerb defines the restful api http verb
 type HTTPVerb string
@@ -118,14 +69,15 @@ type Resource struct {
 
 // Method defines the method of the api
 type Method struct {
-	OnAir          bool `yaml:"onAir"`
-	HTTPVerb       `yaml:"httpVerb"`
-	InboundRequest `yaml:"inboundRequest"`
+	OnAir              bool `yaml:"onAir"`
+	HTTPVerb           `yaml:"httpVerb"`
+	InboundRequest     `yaml:"inboundRequest"`
+	IntegrationRequest `yaml:"integrationRequest"`
 }
 
 // InboundRequest defines the details of the inbound
 type InboundRequest struct {
-	Type         string           `yaml:"type"` //http, TO-DO: dubbo
+	RequestType  string           `yaml:"requestType"` //http, TO-DO: dubbo
 	Headers      []Params         `yaml:"headers"`
 	QueryStrings []Params         `yaml:"queryStrings"`
 	RequestBody  []BodyDefinition `yaml:"requestBody"`
@@ -145,12 +97,13 @@ type BodyDefinition struct {
 
 // IntegrationRequest defines the backend request format and target
 type IntegrationRequest struct {
-	Type                string `yaml:"type"` // dubbo, TO-DO: http
-	dubbo.DubboMetadata `yaml:"dubboMetaData,inline"`
+	RequestType         string         `yaml:"requestType"` // dubbo, TO-DO: http
+	MappingParams       []MappingParam `yaml:"mappingParams,omitempty"`
+	dubbo.DubboMetadata `yaml:"dubboMetaData,inline,omitempty"`
 }
 
-// MappingParams defines the mapping rules of headers and queryStrings
-type MappingParams struct {
+// MappingParam defines the mapping rules of headers and queryStrings
+type MappingParam struct {
 	Name  string `yaml:"name"`
 	MapTo string `yaml:"mapTo"`
 }
