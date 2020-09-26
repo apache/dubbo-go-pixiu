2d85bfc363f934ba6062ed66802c72c99f28c9be
yangqing.xyq <yangqing.xyq@gongdao.com>
2020-09-26 11:05:01 +0800

    add httpclient

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,8 +1,44 @@
 package client
 
+import (
+	"errors"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/client/dubbo"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/client/httpclient"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/model"
+	_ "github.com/gin-gonic/gin"
+	"sync"
+)
+
 type Client interface {
 	Init() error
 	Close() error
-
 	Call(req *Request) (resp Response, err error)
+}
+
+type ClientPool struct {
+	poolMap map[model.ApiType]*sync.Pool
+}
+
+func (pool *ClientPool) getClient(t model.ApiType) (Client, error) {
+	if pool.poolMap[t] != nil {
+		return pool.poolMap[t].Get().(Client), nil
+	}
+	return nil, errors.New("protocol not supported yet")
+}
+
+func NewClientPool() *ClientPool {
+	clientPool := &ClientPool{
+		poolMap: make(map[model.ApiType]*sync.Pool),
+	}
+	clientPool.poolMap[model.DUBBO] = &sync.Pool{
+		New: func() interface{} {
+			return dubbo.NewDubboClient()
+		},
+	}
+	clientPool.poolMap[model.REST] = &sync.Pool{
+		New: func() interface{} {
+			return httpclient.NewHttpClient()
+		},
+	}
+	return clientPool
 }
