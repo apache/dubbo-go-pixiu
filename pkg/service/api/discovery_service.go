329765cc4465769f928d8e49ce2aae8dda4b5411
williamfeng323 <williamfeng323@hotmail.com>
2020-10-08 01:06:54 +0800

    load the API router config into the local memory

--- pkg/service/api/discovery_service.go
+++ pkg/service/api/discovery_service.go
@@ -19,34 +19,44 @@ package api
 
 import (
 	"encoding/json"
-	"errors"
 )
 
 import (
 	"github.com/goinggo/mapstructure"
+	"github.com/pkg/errors"
 )
 
 import (
+	"fmt"
 	"github.com/dubbogo/dubbo-go-proxy/pkg/client/dubbo"
 	"github.com/dubbogo/dubbo-go-proxy/pkg/common/constant"
 	"github.com/dubbogo/dubbo-go-proxy/pkg/common/extension"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/config"
 	"github.com/dubbogo/dubbo-go-proxy/pkg/logger"
 	"github.com/dubbogo/dubbo-go-proxy/pkg/model"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/router"
 	"github.com/dubbogo/dubbo-go-proxy/pkg/service"
+	"strings"
 )
 
 func init() {
-	extension.SetApiDiscoveryService(constant.LocalMemoryApiDiscoveryService, NewLocalMemoryApiDiscoveryService())
+	extension.SetApiDiscoveryService(constant.LocalMemoryApiDiscoveryService, NewLocalMemoryAPIDiscoveryService())
 }
 
-type LocalMemoryApiDiscoveryService struct {
+// LocalMemoryAPIDiscoveryService is the local cached API discovery service
+type LocalMemoryAPIDiscoveryService struct {
+	router *router.Route
 }
 
-func NewLocalMemoryApiDiscoveryService() *LocalMemoryApiDiscoveryService {
-	return &LocalMemoryApiDiscoveryService{}
+// NewLocalMemoryAPIDiscoveryService creates a new LocalMemoryApiDiscoveryService instance
+func NewLocalMemoryAPIDiscoveryService() *LocalMemoryAPIDiscoveryService {
+	return &LocalMemoryAPIDiscoveryService{
+		router: router.NewRoute(),
+	}
 }
 
-func (ads *LocalMemoryApiDiscoveryService) AddApi(request service.DiscoveryRequest) (service.DiscoveryResponse, error) {
+// AddApi adds a method to the router tree
+func (ads *LocalMemoryAPIDiscoveryService) AddApi(request service.DiscoveryRequest) (service.DiscoveryResponse, error) {
 	aj := model.NewApi()
 	if err := json.Unmarshal(request.Body, aj); err != nil {
 		return *service.EmptyDiscoveryResponse, err
@@ -77,7 +87,7 @@ func (ads *LocalMemoryApiDiscoveryService) AddApi(request service.DiscoveryReque
 	return *service.NewDiscoveryResponseWithSuccess(true), nil
 }
 
-func (ads *LocalMemoryApiDiscoveryService) GetApi(request service.DiscoveryRequest) (service.DiscoveryResponse, error) {
+func (ads *LocalMemoryAPIDiscoveryService) GetApi(request service.DiscoveryRequest) (service.DiscoveryResponse, error) {
 	n := string(request.Body)
 
 	if a, ok := model.CacheApi.Load(n); ok {
@@ -85,4 +95,75 @@ func (ads *LocalMemoryApiDiscoveryService) GetApi(request service.DiscoveryReque
 	}
 
 	return *service.EmptyDiscoveryResponse, errors.New("not found")
+}
+
+// AddAPI adds a method to the router tree
+func (ads *LocalMemoryAPIDiscoveryService) AddAPI(api service.API) error {
+	return ads.router.Put(api.URLPattern, api.Method)
+}
+
+// GetAPI returns the method to the caller
+func (ads *LocalMemoryAPIDiscoveryService) GetAPI(url string, httpVerb config.HTTPVerb) (service.DiscoveryResponse, error) {
+	if method, ok := ads.router.FindMethod(url, httpVerb); ok {
+		return *service.NewDiscoveryResponse(method), nil
+	}
+
+	return *service.EmptyDiscoveryResponse, errors.New("not found")
+}
+
+// InitAPIsFromConfig inits the router from API config and to local cache
+func InitAPIsFromConfig(apiConfig *config.APIConfig) error {
+	localAPIDiscSrv := extension.GetMustApiDiscoveryService(constant.LocalMemoryApiDiscoveryService)
+	if len(apiConfig.Resources) == 0 {
+		return nil
+	}
+	return loadAPIFromResource("", apiConfig.Resources, localAPIDiscSrv)
+}
+
+func loadAPIFromResource(parrentPath string, resources []config.Resource, localSrv service.ApiDiscoveryService) error {
+	errStack := []string{}
+	if len(resources) == 0 {
+		return nil
+	}
+	groupPath := parrentPath
+	if parrentPath == constant.PathSlash {
+		groupPath = ""
+	}
+	for _, resource := range resources {
+		fullPath := groupPath + resource.Path
+		if !strings.HasPrefix(resource.Path, constant.PathSlash) {
+			errStack = append(errStack, fmt.Sprintf("Path %s in %s doesn't start with /", resource.Path, parrentPath))
+			continue
+		}
+		if len(resource.Resources) > 0 {
+			if err := loadAPIFromResource(resource.Path, resource.Resources, localSrv); err != nil {
+				errStack = append(errStack, err.Error())
+			}
+		}
+
+		if err := loadAPIFromMethods(fullPath, resource.Methods, localSrv); err != nil {
+			errStack = append(errStack, err.Error())
+		}
+	}
+	if len(errStack) > 0 {
+		return errors.New(strings.Join(errStack, "; "))
+	}
+	return nil
+}
+
+func loadAPIFromMethods(fullPath string, methods []config.Method, localSrv service.ApiDiscoveryService) error {
+	errStack := []string{}
+	for _, method := range methods {
+		api := service.API{
+			URLPattern: fullPath,
+			Method:     method,
+		}
+		if err := localSrv.AddAPI(api); err != nil {
+			errStack = append(errStack, fmt.Sprintf("Path: %s, Method: %s, error: %s", fullPath, method.HTTPVerb, err.Error()))
+		}
+	}
+	if len(errStack) > 0 {
+		return errors.New(strings.Join(errStack, "\n"))
+	}
+	return nil
 }
---
+++ pkg/service/api/discovery_service_test.go
@@ -0,0 +1,195 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package api
+
+import (
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+import (
+	"github.com/dubbogo/dubbo-go-proxy/pkg/common/constant"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/common/extension"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/config"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/service"
+)
+
+func getMockAPI(urlPattern string, verb config.HTTPVerb) service.API {
+	inbound := config.InboundRequest{}
+	integration := config.IntegrationRequest{}
+	method := config.Method{
+		OnAir:              true,
+		HTTPVerb:           verb,
+		InboundRequest:     inbound,
+		IntegrationRequest: integration,
+	}
+	return service.API{
+		URLPattern: urlPattern,
+		Method:     method,
+	}
+}
+
+func TestNewLocalMemoryAPIDiscoveryService(t *testing.T) {
+	l := NewLocalMemoryAPIDiscoveryService()
+	assert.NotNil(t, l)
+	assert.NotNil(t, l.router)
+}
+
+func TestAddAPI(t *testing.T) {
+	l := NewLocalMemoryAPIDiscoveryService()
+	err := l.AddAPI(getMockAPI("/this/is/test", config.MethodPut))
+	assert.Nil(t, err)
+	_, found := l.router.FindMethod("/this/is/test", config.MethodPut)
+	assert.True(t, found)
+}
+
+func TestGetAPI(t *testing.T) {
+	l := NewLocalMemoryAPIDiscoveryService()
+	err := l.AddAPI(getMockAPI("/this/is/test", config.MethodPut))
+	assert.Nil(t, err)
+	_, err = l.GetAPI("/this/is/test", config.MethodPut)
+	assert.Nil(t, err)
+
+	_, err = l.GetAPI("/this/is/test/or/else", config.MethodPut)
+	assert.NotNil(t, err)
+}
+
+func TestLoadAPI(t *testing.T) {
+	apiC, err := config.LoadAPIConfigFromFile("../../config/mock/api_config.yml")
+	assert.Empty(t, err)
+	err = InitAPIsFromConfig(apiC)
+	assert.Nil(t, err)
+	apiDisSrv := extension.GetMustApiDiscoveryService(constant.LocalMemoryApiDiscoveryService)
+	rsp, err := apiDisSrv.GetAPI("/", config.MethodGet)
+	assert.True(t, rsp.Success)
+	rsp, err = apiDisSrv.GetAPI("/mockTest", config.MethodGet)
+	assert.True(t, rsp.Success)
+	rsp, err = apiDisSrv.GetAPI("/mockTest", config.MethodPost)
+	assert.True(t, rsp.Success)
+	rsp, err = apiDisSrv.GetAPI("/mockTest/12345", config.MethodGet)
+	assert.True(t, rsp.Success)
+}
+
+func TestLoadAPIFromResource(t *testing.T) {
+	apiDiscSrv := NewLocalMemoryAPIDiscoveryService()
+	mockMethod1 := getMockAPI("", config.MethodPut).Method
+	mockMethod2 := getMockAPI("", config.MethodPost).Method
+	mockMethod3 := getMockAPI("", config.MethodGet).Method
+	tempResources := []config.Resource{
+		{
+			Type:        "Restful",
+			Path:        "/",
+			Description: "test only",
+			Methods: []config.Method{
+				mockMethod1,
+				mockMethod2,
+				mockMethod3,
+			},
+			Resources: []config.Resource{
+				{
+					Type:        "Restful",
+					Path:        "/mock",
+					Description: "test only",
+					Methods: []config.Method{
+						mockMethod1,
+						mockMethod2,
+						mockMethod3,
+					},
+				},
+				{
+					Type:        "Restful",
+					Path:        "/mock2",
+					Description: "test only",
+					Methods: []config.Method{
+						mockMethod1,
+					},
+					Resources: []config.Resource{
+						{
+							Type:        "Restful",
+							Path:        "/:id",
+							Description: "test only",
+							Methods: []config.Method{
+								mockMethod1,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	err := loadAPIFromResource("", tempResources, apiDiscSrv)
+	assert.Nil(t, err)
+	rsp, _ := apiDiscSrv.GetAPI("/", config.MethodPut)
+	assert.True(t, rsp.Success)
+	rsp, _ = apiDiscSrv.GetAPI("/", config.MethodGet)
+	assert.True(t, rsp.Success)
+	rsp, _ = apiDiscSrv.GetAPI("/mock", config.MethodGet)
+	assert.True(t, rsp.Success)
+	rsp, _ = apiDiscSrv.GetAPI("/mock2/12345", config.MethodPut)
+	assert.True(t, rsp.Success)
+
+	tempResources = []config.Resource{
+		{
+			Type:        "Restful",
+			Path:        "/mock",
+			Description: "test only",
+			Methods: []config.Method{
+				mockMethod1,
+			},
+			Resources: []config.Resource{
+				{
+					Type:        "Restful",
+					Path:        ":id",
+					Description: "test only",
+					Methods: []config.Method{
+						mockMethod1,
+					},
+				},
+				{
+					Type:        "Restful",
+					Path:        ":ik",
+					Description: "test only",
+					Methods: []config.Method{
+						mockMethod1,
+					},
+				},
+			},
+		},
+	}
+	apiDiscSrv = NewLocalMemoryAPIDiscoveryService()
+	err = loadAPIFromResource("", tempResources, apiDiscSrv)
+	assert.EqualError(t, err, "Path :id in /mock doesn't start with /; Path :ik in /mock doesn't start with /")
+}
+
+func TestLoadAPIFromMethods(t *testing.T) {
+	tempMethods := []config.Method{
+		getMockAPI("", config.MethodPut).Method,
+		getMockAPI("", config.MethodGet).Method,
+		getMockAPI("", config.MethodPut).Method,
+	}
+	apiDiscSrv := NewLocalMemoryAPIDiscoveryService()
+	err := loadAPIFromMethods("/mock", tempMethods, apiDiscSrv)
+	rsp, _ := apiDiscSrv.GetAPI("/mock", config.MethodPut)
+	assert.True(t, rsp.Success)
+	rsp, _ = apiDiscSrv.GetAPI("/mock", config.MethodGet)
+	assert.True(t, rsp.Success)
+	assert.EqualError(t, err, "Path: /mock, Method: PUT, error: Method PUT already exists in path /mock")
+}
