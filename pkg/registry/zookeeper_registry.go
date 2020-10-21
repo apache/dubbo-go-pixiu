432fdab2cc13d1793cb55ea60f42d2d4037a0029
shen <1292369127@qq.com>
2020-10-21 17:37:30 +0800

    zookeeper registry

---
+++ pkg/registry/zookeeper_registry.go
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package registry
+
+import (
+	"path"
+	"strings"
+	"time"
+)
+
+import (
+	"github.com/dubbogo/dubbo-go-proxy/pkg/logger"
+)
+
+import (
+	"github.com/apache/dubbo-go/common"
+	"github.com/apache/dubbo-go/remoting/zookeeper"
+)
+
+func init() {
+	var _ RegistryLoad = new(ZookeeperRegistryLoad)
+}
+
+// ConsulRegistryLoad load dubbo apis from consul registry
+type ZookeeperRegistryLoad struct {
+	zkName  string
+	zkPath  map[string]int
+	client  *zookeeper.ZookeeperClient
+	Address string
+	cluster string
+}
+
+func newZookeeperRegistryLoad(address, cluster string) (RegistryLoad, error) {
+	newClient, err := zookeeper.NewZookeeperClient("zkClient", strings.Split(address, ","), 15*time.Second)
+	if err != nil {
+		logger.Warnf("newZookeeperClient error:%v", err)
+		return nil, err
+	}
+
+	r := &ZookeeperRegistryLoad{
+		Address: address,
+		client:  newClient,
+		cluster: cluster,
+	}
+
+	return r, nil
+}
+
+func (crl *ZookeeperRegistryLoad) GetCluster() (string, error) {
+	return crl.cluster, nil
+}
+
+// LoadAllServices load all services from consul registry
+func (crl *ZookeeperRegistryLoad) LoadAllServices() ([]common.URL, error) {
+	rootPath := "/dubbo/"
+	children, err := crl.client.GetChildren(rootPath)
+	if err != nil {
+		logger.Errorf("[zookeeper registry] get zk children error:%v", err)
+		return nil, err
+	}
+	var urls []common.URL
+	for _, child := range children {
+		providerStr := path.Join(rootPath, "/", child, "/", "providers")
+		urlStrs, err := crl.client.GetChildren(providerStr)
+		if err != nil {
+			logger.Errorf("[zookeeper registry] get zk children \"%s\" error:%v", providerStr, err)
+			return nil, err
+		}
+		for _, url := range urlStrs {
+			dubboUrl, err := common.NewURL(url)
+			if err != nil {
+				logger.Warnf("[zookeeper registry] transfer zk info to url error:%v", err)
+				continue
+			}
+			urls = append(urls, dubboUrl)
+		}
+	}
+	return urls, nil
+}
