cdd49f71c46e8d0c4c8ff49914d9b30d39d3c2ef
shen <1292369127@qq.com>
2020-10-24 16:21:07 +0800

    zookeeper registry unit test

--- pkg/registry/zookeeper_registry_test.go
+++ pkg/registry/zookeeper_registry_test.go
@@ -18,14 +18,6 @@ package registry
 
 import (
 	"fmt"
-	"github.com/apache/dubbo-go/common"
-	"github.com/apache/dubbo-go/common/constant"
-	zkRegistry "github.com/apache/dubbo-go/registry"
-	"github.com/apache/dubbo-go/remoting/zookeeper"
-	"github.com/dubbogo/dubbo-go-proxy/pkg/logger"
-	"github.com/dubbogo/go-zookeeper/zk"
-	perrors "github.com/pkg/errors"
-	"github.com/stretchr/testify/assert"
 	"net/url"
 	"strconv"
 	"strings"
@@ -33,26 +25,34 @@ import (
 	"time"
 )
 
-func newMockZkRegistry(providerUrl common.URL, opts ...zookeeper.Option) (*zk.TestCluster, error) {
+import (
+	"github.com/dubbogo/go-zookeeper/zk"
+)
+
+import (
+	"github.com/apache/dubbo-go/common"
+	"github.com/apache/dubbo-go/common/constant"
+	"github.com/apache/dubbo-go/remoting/zookeeper"
+	"github.com/stretchr/testify/assert"
+)
+
+const providerUrlstr = "dubbo://127.0.0.1:20000/com.ikurento.user.UserProvider?methods.GetUser.retries=1"
+
+func newMockZkRegistry(t *testing.T, providerUrl common.URL, opts ...zookeeper.Option) (*zk.TestCluster, error) {
 	var (
-		err      error
-		c        *zk.TestCluster
-		client   *zookeeper.ZookeeperClient
-		registry *zkRegistry.BaseRegistry
-		//event <-chan zk.Event
+		err    error
+		c      *zk.TestCluster
+		client *zookeeper.ZookeeperClient
 	)
 	c, client, _, err = zookeeper.NewMockZookeeperClient("test", 15*time.Second, opts...)
 	if err != nil {
 		return nil, err
 	}
 	var (
-		//revision   string
 		params     url.Values
 		rawURL     string
-		zkPath     string
 		encodedURL string
 		dubboPath  string
-		//conf       config.URL
 	)
 	params = url.Values{}
 
@@ -61,17 +61,12 @@ func newMockZkRegistry(providerUrl common.URL, opts ...zookeeper.Option) (*zk.Te
 		return true
 	})
 
-	params.Add("pid", "")
-	params.Add("ip", "")
-	//params.Add("timeout", fmt.Sprintf("%d", int64(r.Timeout)/1e6))
+	dubboPath = fmt.Sprintf("/dubbo/%s/%s", url.QueryEscape(providerUrl.Service()), common.DubboNodes[common.PROVIDER])
+	err = client.CreateWithValue(dubboPath, []byte(""))
+	assert.Nil(t, err)
 
-	role, _ := strconv.Atoi(registry.URL.GetParam(constant.ROLE_KEY, ""))
-	switch role {
-
-	case common.PROVIDER:
-		dubboPath = fmt.Sprintf("/dubbo/%s/%s", url.QueryEscape(providerUrl.Service()), common.DubboNodes[common.PROVIDER])
-		client.CreateWithValue(dubboPath, []byte(""))
-
+	if len(providerUrl.Methods) > 0 {
+		params.Add(constant.METHODS_KEY, strings.Join(providerUrl.Methods, ","))
 	}
 	var host string
 	if providerUrl.Ip == "" {
@@ -79,53 +74,51 @@ func newMockZkRegistry(providerUrl common.URL, opts ...zookeeper.Option) (*zk.Te
 	} else {
 		host = providerUrl.Ip
 	}
-	rawURL = fmt.Sprintf("%s://%s%s?%s", providerUrl.Protocol, host, c.Path, params.Encode())
+	host += ":" + providerUrl.Port
+	rawURL = fmt.Sprintf("%s://%s%s?%s", providerUrl.Protocol, host, providerUrl.Path, params.Encode())
 
 	encodedURL = url.QueryEscape(rawURL)
 	dubboPath = strings.ReplaceAll(dubboPath, "$", "%24")
 
 	err = client.Create(dubboPath)
-	if err != nil {
-		return nil, perrors.WithStack(err)
-	}
+	assert.Nil(t, err)
 
-	// try to register the node
-	zkPath, err = client.RegisterTemp(dubboPath, encodedURL)
-	if err != nil {
-		logger.Errorf("Register temp node(root{%s}, node{%s}) = error{%v}", dubboPath, encodedURL, perrors.WithStack(err))
-		if perrors.Cause(err) == zk.ErrNodeExists {
-			// should delete the old node
-			logger.Info("Register temp node failed, try to delete the old and recreate  (root{%s}, node{%s}) , ignore!", dubboPath, encodedURL)
-			if err = client.Delete(zkPath); err == nil {
-				_, err = client.RegisterTemp(dubboPath, encodedURL)
-			}
-			if err != nil {
-				logger.Errorf("Recreate the temp node failed, (root{%s}, node{%s}) = error{%v}", dubboPath, encodedURL, perrors.WithStack(err))
-			}
-		}
-		return nil, perrors.WithMessagef(err, "RegisterTempNode(root{%s}, node{%s})", dubboPath, encodedURL)
-	}
-	logger.Debugf("Create a zookeeper node:%s", zkPath)
-	defer c.Stop()
+	// to register the node
+	_, err = client.RegisterTemp(dubboPath, encodedURL)
+	assert.Nil(t, err)
 	return c, nil
 }
 
-func registerProvider() (*zk.TestCluster, error) {
-	providerUrl, _ := common.NewURL("dubbo://127.0.0.1:20000/com.ikurento.user.UserProvider", common.WithParamsValue(constant.CLUSTER_KEY, "mock"), common.WithParamsValue("serviceid", "soa.mock"), common.WithMethods([]string{"GetUser", "AddUser"}))
-	return newMockZkRegistry(providerUrl)
+func registerProvider(providerUrl common.URL, t *testing.T) (*zk.TestCluster, error) {
+	return newMockZkRegistry(t, providerUrl)
 }
 
 func TestZookeeperRegistryLoad_GetCluster(t *testing.T) {
-	testCluster, err := registerProvider()
+	providerUrl, _ := common.NewURL(providerUrlstr)
+	testCluster, err := registerProvider(providerUrl, t)
 	assert.Nil(t, err)
 	defer testCluster.Stop()
 	registryLoad, err := newZookeeperRegistryLoad("127.0.0.1:1111", "test-cluster")
 	assert.Nil(t, err)
 	assert.NotNil(t, registryLoad)
 	clusterName, err := registryLoad.GetCluster()
-	assert.NotNil(t, err)
+	assert.Nil(t, err)
 	assert.Equal(t, "test-cluster", clusterName)
 }
 
 func TestZookeeperRegistryLoad_LoadAllServices(t *testing.T) {
+	providerUrl, _ := common.NewURL(providerUrlstr)
+	testCluster, err := registerProvider(providerUrl, t)
+	assert.Nil(t, err)
+	defer testCluster.Stop()
+	registryLoad, err := newZookeeperRegistryLoad(fmt.Sprintf("%s:%s", "127.0.0.1", strconv.Itoa(testCluster.Servers[0].Port)), "test-cluster")
+	assert.Nil(t, err)
+	assert.NotNil(t, registryLoad)
+	services, err := registryLoad.LoadAllServices()
+	assert.Nil(t, err)
+	assert.GreaterOrEqual(t, len(services), 1)
+	assert.Equal(t, services[0].Protocol, "dubbo")
+	assert.Equal(t, services[0].Location, "127.0.0.1:20000")
+	assert.Equal(t, services[0].Path, "/com.ikurento.user.UserProvider")
+	assert.Equal(t, services[0].GetMethodParam("GetUser", "retries", ""), "1")
 }
