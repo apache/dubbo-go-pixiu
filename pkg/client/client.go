acd34ba7ad6b8e1e9e8a364c1850419a903b6dc6
tiecheng <ironcity.hz@gmail.com>
2020-11-08 16:52:07 +0800

    add sample dubbogo.

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -21,4 +21,7 @@ type Client interface {
 	Init() error
 	Close() error
 	Call(req *Request) (resp Response, err error)
+
+	// MappingParams mapping param, uri, query, body ...
+	MappingParams(req *Request) (types []string, reqData []interface{}, err error)
 }
