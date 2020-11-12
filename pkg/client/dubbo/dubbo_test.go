da840a4349b2fb007eac448abc507a0e6ff19980
williamfeng323 <williamfeng323@hotmail.com>
2020-11-12 14:14:28 +0800

    #41 implement mapping inbound params to backend dubbo server
    Former-commit-id: 382f7bbf134fb75de8afa659e1ff4463bfaede19 [formerly 00fac0cce2406dc978aaa422349d204b64452079]
    Former-commit-id: 382f7bbf134fb75de8afa659e1ff4463bfaede19
    Former-commit-id: 04a49b7057091c541170527a2bc01abc3a93af83

--- pkg/client/dubbo/dubbo_test.go
+++ pkg/client/dubbo/dubbo_test.go
@@ -18,12 +18,20 @@
 package dubbo
 
 import (
+	"bytes"
+	"net/http"
 	"regexp"
 	"testing"
 )
 
 import (
 	"github.com/stretchr/testify/assert"
+)
+
+import (
+	"github.com/dubbogo/dubbo-go-proxy/pkg/client"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/common/mock"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/config"
 )
 
 func TestReg(t *testing.T) {
@@ -78,4 +86,118 @@ func TestClose(t *testing.T) {
 	assert.Equal(t, 4, len(client.GenericServicePool))
 	client.Close()
 	assert.Equal(t, 0, len(client.GenericServicePool))
+}
+
+func TestMappingParams(t *testing.T) {
+	dClient := NewDubboClient()
+	r, _ := http.NewRequest("GET", "/mock/test?id=12345&age=19", bytes.NewReader([]byte("")))
+	api := mock.GetMockAPI(config.MethodGet, "/mock/test")
+	api.IntegrationRequest.MappingParams = []config.MappingParam{
+		{
+			Name:  "queryStrings.id",
+			MapTo: "0",
+		},
+		{
+			Name:  "queryStrings.age",
+			MapTo: "1",
+		},
+	}
+	req := client.NewReq(r, api)
+	_, params, err := dClient.MappingParams(req)
+	assert.Nil(t, err)
+	assert.Equal(t, params[0], "12345")
+	assert.Equal(t, params[1], "19")
+
+	r, _ = http.NewRequest("GET", "/mock/test?id=12345&age=19", bytes.NewReader([]byte("")))
+	api = mock.GetMockAPI(config.MethodGet, "/mock/test")
+	api.IntegrationRequest.MappingParams = []config.MappingParam{
+		{
+			Name:  "queryStrings.id",
+			MapTo: "0",
+		},
+		{
+			Name:  "queryStrings.age",
+			MapTo: "1",
+		},
+		{
+			Name:  "headers.Auth",
+			MapTo: "2",
+		},
+	}
+	r.Header.Set("Auth", "1234567")
+	req = client.NewReq(r, api)
+	_, params, err = dClient.MappingParams(req)
+	assert.Nil(t, err)
+	assert.Equal(t, params[0], "12345")
+	assert.Equal(t, params[1], "19")
+	assert.Equal(t, params[2], "1234567")
+
+	r, _ = http.NewRequest("POST", "/mock/test?id=12345&age=19", bytes.NewReader([]byte(`{"sex": "male", "name":{"firstName": "Joe", "lastName": "Biden"}}`)))
+	api = mock.GetMockAPI(config.MethodGet, "/mock/test")
+	api.IntegrationRequest.MappingParams = []config.MappingParam{
+		{
+			Name:  "queryStrings.id",
+			MapTo: "0",
+		},
+		{
+			Name:  "queryStrings.age",
+			MapTo: "1",
+		},
+		{
+			Name:  "headers.Auth",
+			MapTo: "2",
+		},
+		{
+			Name:  "requestBody.sex",
+			MapTo: "3",
+		},
+		{
+			Name:  "requestBody.name.firstName",
+			MapTo: "4",
+		},
+	}
+	r.Header.Set("Auth", "1234567")
+	req = client.NewReq(r, api)
+	_, params, err = dClient.MappingParams(req)
+	assert.Nil(t, err)
+	assert.Equal(t, params[0], "12345")
+	assert.Equal(t, params[1], "19")
+	assert.Equal(t, params[2], "1234567")
+	assert.Equal(t, params[3], "male")
+	assert.Equal(t, params[4], "Joe")
+}
+
+func TestValidateTarget(t *testing.T) {
+	target := []interface{}{}
+	val, err := validateTarget(&target)
+	assert.Nil(t, err)
+	assert.NotNil(t, val)
+	_, err = validateTarget(target)
+	assert.EqualError(t, err, "Target params must be a non-nil pointer")
+	target2 := ""
+	_, err = validateTarget(&target2)
+	assert.EqualError(t, err, "Target params for dubbo backend must be *[]interface{}")
+}
+
+func TestParseMapSource(t *testing.T) {
+	from, key, err := client.ParseMapSource("queryStrings.id")
+	assert.Nil(t, err)
+	assert.Equal(t, from, "queryStrings")
+	assert.Equal(t, key[0], "id")
+
+	from, key, err = client.ParseMapSource("headers.id")
+	assert.Nil(t, err)
+	assert.Equal(t, from, "headers")
+	assert.Equal(t, key[0], "id")
+
+	from, key, err = client.ParseMapSource("requestBody.user.id")
+	assert.Nil(t, err)
+	assert.Equal(t, from, "requestBody")
+	assert.Equal(t, key[0], "user")
+	assert.Equal(t, key[1], "id")
+
+	from, key, err = client.ParseMapSource("what.user.id")
+	assert.EqualError(t, err, "Parameter mapping config incorrect. Please fix it")
+	from, key, err = client.ParseMapSource("requestBody.*userid")
+	assert.EqualError(t, err, "Parameter mapping config incorrect. Please fix it")
 }
