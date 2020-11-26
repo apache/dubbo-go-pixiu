7860209290e64344289820baccc8e59c71f49da4
tiecheng <ironcity.hz@gmail.com>
2020-11-26 10:28:00 +0800

    resolve
    Former-commit-id: 15f6bf5b50fbd4cdba6488ea871f1c68a9c826d3
    Former-commit-id: 8c05c17a380673e485c4808618ca08f172d7a871

--- pkg/filter/recovery/recovery_test.go
+++ pkg/filter/recovery/recovery_test.go
@@ -28,7 +28,7 @@ import (
 )
 
 func TestRecovery(t *testing.T) {
-	c := mock.GetMockHTTPContext(nil,  New().Do(), func(c selfcontext.Context) {
+	c := mock.GetMockHTTPContext(nil, New().Do(), func(c selfcontext.Context) {
 		time.Sleep(time.Millisecond * 100)
 		// panic
 		var m map[string]string
