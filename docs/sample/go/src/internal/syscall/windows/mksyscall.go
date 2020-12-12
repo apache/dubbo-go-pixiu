d5f6a680685564b41fd0d79033eecad9a1f59213
zhangshen023 <1292369127@qq.com>
2020-12-12 10:49:24 +0800

    test mac passed
    Former-commit-id: 6a89b2428825e0033840f69fed3b8604fb8c6efe

--- docs/sample/go/src/internal/syscall/windows/mksyscall.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2016 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// +build generate
-
-package windows
-
-//go:generate go run golang.org/x/sys/windows/mkwinsyscall -output zsyscall_windows.go syscall_windows.go security_windows.go psapi_windows.go symlink_windows.go
