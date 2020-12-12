57e655b0cc5773f61ed9a38e2efe6195d7459969
zhangshen023 <1292369127@qq.com>
2020-12-12 10:49:24 +0800

    test mac passed
    Former-commit-id: d5f6a680685564b41fd0d79033eecad9a1f59213 [formerly 6a89b2428825e0033840f69fed3b8604fb8c6efe]
    Former-commit-id: d5f6a680685564b41fd0d79033eecad9a1f59213
    Former-commit-id: bd9ba7299773c8d8bba1c0a46ef2bb6feb0a78e9

--- docs/sample/dubbo_sample_startup.sh
+++ docs/sample/dubbo_sample_startup.sh
@@ -1,61 +1,31 @@
 #!/bin/sh
-if [ "$1" != "skip_zookeeper" ];then
-	zookeeper_container_id=`docker ps | grep zookeeper  | head -n 1 | awk '{print  $1}'`
-	if [ ! -z "$zookeeper_container_id=" ]; then 
-		echo "removing old zookeeper!"
-        	docker kill $zookeeper_container_id 
-        	docker rm $zookeeper_container_id
-	fi
-	echo "starting zookeeper!"
-	docker run -dit --name zookeeper -p 2181:2181 zookeeper:3.4
-	echo "zookeeper stated!"
+if [ "$1" != "skip_zookeeper" ]; then
+  zookeeper_container_id=$(docker ps | grep zookeeper | head -n 1 | awk '{print  $1}')
+  if [ -n "$zookeeper_container_id" ]; then
+    echo "removing old zookeeper!"
+    docker kill "$zookeeper_container_id"
+    docker rm "$zookeeper_container_id"
+  fi
+  echo "starting zookeeper!"
+  docker run -dit --name zookeeper -p 2181:2181 zookeeper:3.4.14
+  echo "zookeeper stated!"
 fi
 
+go env -w GOPROXY=https://goproxy.cn,direct
 
-platform=$(uname)
-echo "finding go command!"
-if ! command -v go >/dev/null 2>&1; then
-	echo "go not exists"
-	if [ ${platform} == "Darwin" ]; then
-        	go_package="go1.14.13.darwin-amd64.tar.gz"
-	else
-        	go_package="go1.14.13.linux-amd64.tar.gz"
-	fi
-	if [ ! -d go ]; then
-		rm -rf go_sdk.tar.gz
-		download_url="https://golang.org/dl/$go_package"
-        	echo "downloading go sdk: $download_url"
-        	curl --connect-timeout 20 -o go_sdk.tar.gz $download_url
-        	if [ ! -f go_sdk.tar.gz ]; then
-                	echo "download fail! try using go语言中文网"
-                	download_url="https://studygolang.com/dl/golang/$go_package"
-                	echo "downloading go sdk: $download_url"
-                	curl --connect-timeout 20 --location-trusted -o go_sdk.tar.gz $download_url
-        	fi
-        	tar -zxf go_sdk.tar.gz
-	fi
-	go_cmd="go/bin/go"
-else
-	go_root=`go env GOROOOT`
-	go_cmd="$go_root/bin/go"
-fi
-
-absolute_go_cmd=`readlink -f $go_cmd`
-$go_cmd env -w GOPROXY=https://goproxy.cn,direct
-echo "go command is $go_cmd"
 echo "starting dubbogo provider!"
-if [ -z $CONF_PROVIDER_FILE_PATH ]; then
-	export CONF_PROVIDER_FILE_PATH=../../sample/dubbogo/server/config/server.yml
+if [ -z "$CONF_PROVIDER_FILE_PATH" ]; then
+  export CONF_PROVIDER_FILE_PATH=../../sample/dubbogo/server/config/server.yml
 fi
-$go_cmd build ../../sample/dubbogo/server/app/
+go build ../../sample/dubbogo/server/app/
 
-provider_pid=`ps -ef  | grep ../../sample/dubbogo/server/app/ | grep -v 'grep' | awk '{print $2}'`
+provider_pid=$(ps -ef | grep ../../sample/dubbogo/server/app/ | grep -v 'grep' | awk '{print $2}')
 
-if [ ! -n $provider_pid ]; then
-	echo "pid of old dubbogo provider is $provider_pid, kill it"
-	kill -9 $provider_pid
+if [ -n "$provider_pid" ]; then
+  echo "pid of old dubbogo provider is $provider_pid, kill it"
+  kill -9 "$provider_pid"
 fi
-nohup $go_cmd run ../../sample/dubbogo/server/app/ > dubbgo_server.out &
+nohup go run ../../sample/dubbogo/server/app/ >dubbgo_server.out &
 sleep 10
 echo "dubbogo provider started!"
 
@@ -63,7 +33,5 @@ echo "dubbogo provider started!"
 
 echo "starting proxy!"
 
-echo "the absolute path of go command is $absolute_go_cmd"
 cd ../../
-make run go_cmd=$absolute_go_cmd
-
+make run config-path=sample/dubbogo/proxy/conf.yaml api-config-path=sample/dubbogo/proxy/api_config.yaml
