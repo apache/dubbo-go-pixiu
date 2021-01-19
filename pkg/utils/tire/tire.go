ca242911a338471e55432e9b1122e3f9fc2f4816
yangqing.xyq <yangqing.xyq@gongdao.com>
2021-01-19 11:19:09 +0800

    tire for path match.

--- pkg/utils/tire/tire.go
+++ pkg/utils/tire/tire.go
@@ -23,16 +23,16 @@ type Node struct {
 	bizInfo          interface{}      //随便塞啥
 }
 
+//Tire对外暴露 推荐外部使用
+
 func (tire *Tire) Put(withOutHost string, bizInfo interface{}) bool {
 	parts := urlPath.Split(withOutHost)
-
 	return tire.root.Put(parts, bizInfo)
 }
 func (tire Tire) Get(withOutHost string) (*Node, []string, bool) {
 	parts := urlPath.Split(withOutHost)
 	return tire.root.Get(parts)
 }
-
 func (tire Tire) Match(withOutHost string) (*Node, *[]string, bool) {
 	parts := urlPath.Split(withOutHost)
 	return tire.root.Match(parts)
@@ -45,27 +45,34 @@ func (tire Tire) Remove(withOutHost string) {
 		n.endOfPath = false
 	}
 }
-
 func (tire Tire) Contains(withOutHost string) bool {
 	parts := urlPath.Split(withOutHost)
 	ret, _, _ := tire.root.Get(parts)
 	return !(ret == nil)
 }
 
+//不对外暴露，不推荐外部使用
+
 func (node *Node) Put(keys []string, bizInfo interface{}) bool {
+	//空节点初始化
 	if node.children == nil {
 		node.children = map[string]*Node{}
 	}
 	if len(keys) == 0 {
 		return true
 	}
+
 	key := keys[0]
 	isReal := len(keys) == 1
+	//屏蔽 通配和非统配的细节put 方法，不涉及递归
 	isSuccess := node.put(key, isReal, bizInfo)
+	//递归退出条件
 	if !isSuccess {
 		return false
 	}
 	childKeys := keys[1:]
+
+	//递归体
 	if urlPath.IsPathVariable(key) {
 		return node.PathVariableNode.Put(childKeys, bizInfo)
 	} else {
@@ -83,6 +90,7 @@ func (node *Node) Match(parts []string) (*Node, *[]string, bool) {
 	childKeys := parts[1:]
 	isReal := len(childKeys) == 0
 	if isReal {
+		//退出条件
 		if node.children != nil && node.children[key] != nil && node.children[key].endOfPath {
 			return node.children[key], &[]string{}, true
 		}
@@ -94,6 +102,7 @@ func (node *Node) Match(parts []string) (*Node, *[]string, bool) {
 		}
 		return nil, nil, false
 	} else {
+		//递归体
 		if node.children != nil && node.children[key] != nil {
 			node, param, ok := node.children[key].Match(childKeys)
 			if ok {
@@ -118,6 +127,7 @@ func (node *Node) Get(keys []string) (*Node, []string, bool) {
 	childKeys := keys[1:]
 	isReal := len(childKeys) == 0
 	if isReal {
+		//退出条件
 		if urlPath.IsPathVariable(key) {
 			if node.PathVariableNode == nil {
 				return nil, nil, false
@@ -134,6 +144,7 @@ func (node *Node) Get(keys []string) (*Node, []string, bool) {
 			return node.children[key], nil, true
 		}
 	} else {
+		//递归体
 		if urlPath.IsPathVariable(key) {
 			if node.PathVariableNode == nil {
 				return nil, nil, false
@@ -151,6 +162,7 @@ func (node *Node) Get(keys []string) (*Node, []string, bool) {
 }
 
 func (node *Node) put(key string, isReal bool, bizInfo interface{}) bool {
+	//不涉及递归，屏蔽变量 非变量 put 细节
 	if urlPath.IsPathVariable(key) {
 		pathVariable := urlPath.VariableName(key)
 		return node.putPathVariable(pathVariable, isReal, bizInfo)
@@ -160,6 +172,7 @@ func (node *Node) put(key string, isReal bool, bizInfo interface{}) bool {
 }
 
 func (node *Node) putPathVariable(pathVariable string, isReal bool, bizInfo interface{}) bool {
+	//变量put
 	if node.PathVariableNode == nil {
 		node.PathVariableNode = &Node{endOfPath: false}
 	}
@@ -179,6 +192,7 @@ func (node *Node) putPathVariable(pathVariable string, isReal bool, bizInfo inte
 }
 
 func (node *Node) putNode(matchStr string, isReal bool, bizInfo interface{}) bool {
+	//普通node put
 	selfNode := &Node{endOfPath: isReal, matchStr: matchStr}
 	old := node.children[matchStr]
 	if old != nil {
