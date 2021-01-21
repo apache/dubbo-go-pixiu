7977a54e36fe117c96b0c7d2824f257967355f3b
Randy <ztelur@gmail.com>
2021-01-21 19:55:21 +0800

    update
    Former-commit-id: 42f23b1f94ac83081ebc796cafdea62b5079bbba [formerly f321f25071c6b14be061f16fd9d19ca1476706e2 [formerly 65c9f3c4dc8ae95907a2e0131fe51f8e08000b66]]
    Former-commit-id: 6f819a31141a8d259779118026f0a2632942031e [formerly f321f25071c6b14be061f16fd9d19ca1476706e2]
    Former-commit-id: ef0ef6151989decf7befeaa1968e55bbd45a651b [formerly 4724d509254a2f2f76cbca289c698f67bb0cb4c7]
    Former-commit-id: bdb08ef32ccc2c06daada28d9dc7b9dabbfbf3fc

--- cmd/admin/admin.go
+++ cmd/admin/admin.go
@@ -1,49 +1,144 @@
 package main
 
 import (
-	"fmt"
-	"github.com/dubbogo/dubbo-go-proxy/pkg/common/yaml"
-	"github.com/dubbogo/dubbo-go-proxy/pkg/config"
-	etcdv3 "github.com/dubbogo/dubbo-go-proxy/pkg/remoting/etcd3"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-var client *etcdv3.Client
-
-
-const (
-	// EtcdAddress etcd address
-	EtcdAddress   string = "127.0.0.1:2379"
-	// AdminAddress admin server host and port
-	AdminAddress  string = "127.0.0.1:8080"
-	// APIConfigPath api config path in etcd
-	APIConfigPath string = "/proxy/config/api"
+import (
+	etcdv3 "github.com/dubbogo/dubbo-go-proxy/pkg/remoting/etcd3"
+	perrors "github.com/pkg/errors"
+	"github.com/urfave/cli"
 )
 
-func main() {
+import (
+	"github.com/dubbogo/dubbo-go-proxy/pkg/common/yaml"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/config"
+	"github.com/dubbogo/dubbo-go-proxy/pkg/logger"
+)
 
+
+type AdminBootstrap struct {
+	Address  string  `yaml:"address" json:"address" mapstructure:"address"`
+	EtcdConfig EtcdConfig `yaml:"etcd" json:"etcd" mapstructure:"etcd"`
+}
+
+type EtcdConfig struct {
+	Address  string  `yaml:"admin" json:"admin" mapstructure:"admin"`
+	Path string  `yaml:"path" json:"path" mapstructure:"path"`
+}
+
+
+
+var (
+	cmdStart = cli.Command{
+		Name:  "start",
+		Usage: "start dubbogo proxy admin",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:   "config, c",
+				Usage:  "Load configuration from `FILE`",
+				EnvVar: "DUBBOGO_PROXY_CONFIG",
+				Value:  "configs/conf.yaml",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			configPath := c.String("config")
+			_ , err := LoadAPIConfigFromFile(configPath)
+			if err != nil {
+				logger.Errorf("load admin config  error:%+v", err)
+				return err
+			}
+			Start()
+			return nil
+		},
+	}
+
+	cmdStop = cli.Command{
+		Name:  "stop",
+		Usage: "stop dubbogo proxy",
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+	}
+)
+
+
+func newAdminApp(startCmd *cli.Command) *cli.App {
+	app := cli.NewApp()
+	app.Name = "dubbogo proxy admin"
+	app.Version = "0.0.1"
+	app.Compiled = time.Now()
+	app.Copyright = "(c) " + strconv.Itoa(time.Now().Year()) + " Dubbogo"
+	app.Usage = "Dubbogo proxy is a lightweight gateway."
+	app.Flags = cmdStart.Flags
+
+	//commands
+	app.Commands = []cli.Command{
+		cmdStart,
+		cmdStop,
+	}
+
+	//action
+	app.Action = func(c *cli.Context) error {
+		if c.NumFlags() == 0 {
+			return cli.ShowAppHelp(c)
+		}
+		return startCmd.Action.(func(c *cli.Context) error)(c)
+	}
+
+	return app
+}
+
+// LoadAPIConfigFromFile load config from file
+func LoadAPIConfigFromFile(path string) (*AdminBootstrap, error) {
+	if len(path) == 0 {
+		return nil, perrors.Errorf("Config file not specified")
+	}
+	adminBootstrap := &AdminBootstrap{}
+	err := yaml.UnmarshalYMLConfig(path, adminBootstrap)
+	if err != nil {
+		return nil, perrors.Errorf("unmarshalYmlConfig error %v", perrors.WithStack(err))
+	}
+	bootstrap = adminBootstrap;
+	return adminBootstrap, nil
+}
+
+
+func main() {
+	app := newAdminApp(&cmdStart)
+	// ignore error so we don't exit non-zero and break gfmrun README example tests
+	_ = app.Run(os.Args)
+}
+
+
+var client *etcdv3.Client
+var bootstrap *AdminBootstrap
+
+// Start start init etcd client and start admin http server
+func Start() {
 	client = etcdv3.NewConfigClient(
 		etcdv3.WithName(etcdv3.RegistryETCDV3Client),
 		etcdv3.WithTimeout(10*time.Second),
-		etcdv3.WithEndpoints(strings.Split(EtcdAddress, ",")...),
+		etcdv3.WithEndpoints(strings.Split(bootstrap.EtcdConfig.Address, ",")...),
 	)
 	defer client.Close()
 
 	http.HandleFunc("/config/api", GetAPIConfig)
 	http.HandleFunc("/config/api/set", SetAPIConfig)
 
-	http.ListenAndServe(AdminAddress, nil)
-
+	http.ListenAndServe(bootstrap.Address, nil)
 }
 
 // GetAPIConfig handle get api config http request
 func GetAPIConfig(w http.ResponseWriter, req *http.Request) {
-	config, err := client.Get(APIConfigPath)
+	config, err := client.Get(bootstrap.EtcdConfig.Path)
 	if err != nil {
-		fmt.Printf(err.Error())
+		logger.Errorf("GetAPIConfig err, %v\n", err)
 		w.Write([]byte("Error"))
 	}
 	w.Write([]byte(config))
@@ -54,7 +149,7 @@ func SetAPIConfig(w http.ResponseWriter, req *http.Request) {
 
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		fmt.Printf("read body err, %v\n", err)
+		logger.Errorf("read body err, %v\n", err)
 		return
 	}
 	// validate the api config
@@ -62,16 +157,15 @@ func SetAPIConfig(w http.ResponseWriter, req *http.Request) {
 	err = yaml.UnmarshalYML([]byte(body), apiConf)
 
 	if err != nil {
-		fmt.Printf("UnmarshalYML body error, %v", err)
+		logger.Warnf("read body err, %v\n", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	setErr := client.Update(APIConfigPath, string(body))
+	setErr := client.Update(bootstrap.EtcdConfig.Path, string(body))
 	if setErr != nil {
-		fmt.Printf("update etcd error, %v", setErr)
+		logger.Warnf("update etcd error, %v\n", err)
 		w.Write([]byte(setErr.Error()))
 	}
-	fmt.Printf("success")
 	w.Write([]byte("Success"))
 }
