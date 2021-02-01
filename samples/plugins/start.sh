ea4a902ff11a3ea2b8258fa6731bcba68968a1d0
mark4z <anymre@outlook.com>
2021-02-01 17:49:08 +0800

    update samples for plugins

--- samples/plugins/start.sh
+++ samples/plugins/start.sh
@@ -1,17 +1,14 @@
-#!/bin/bash -x
-
-DIR=$(cd $(dirname $0) && pwd)
-
-echo $DIR
+#!/usr/bin/env sh
+DIR=$(cd "$(dirname "$0")" && pwd)
+echo "$DIR"
 
 PLUGIN_DIR=${DIR}/plugin
-
-for f in ${PLUGIN_DIR}/*.go; do
-  echo ${f}
-done
-
 OUT_DIR=${DIR}/out
 
-go build -o ${OUT_DIR} -buildmode=plugin ${PLUGIN_DIR}
+for f in "${PLUGIN_DIR}"/*.go
+do
+  echo "$f"
+done
 
+go build -o "${OUT_DIR}" -buildmode=plugin "${PLUGIN_DIR}"
 echo 'build successful!'
