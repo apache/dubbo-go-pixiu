758096aceb4aad1bda68ccdd65f0fb6c839e7129
xun zhang <18721825717@163.com>
2021-07-02 13:13:49 +0800

    add more test case
    Former-commit-id: c87220732f986c6e938fb68f6a87b5e4706bfe10

--- samples/dubbogo/simple/proxy/test/pixiu_test.go
+++ samples/dubbogo/simple/proxy/test/pixiu_test.go
@@ -30,7 +30,7 @@ import (
 
 func TestPost1(t *testing.T) {
 	url := "http://localhost:8883/api/v1/test-dubbo/UserService/com.dubbogo.pixiu.UserService?group=test&version=1.0.0&method=GetUserByName"
-	data := "{\"types\":[\"string\"],\"values\":\"tc\"}"
+	data := "{\"types\":\"string\",\"values\":\"tc\"}"
 	client := &http.Client{Timeout: 5 * time.Second}
 	req, err := http.NewRequest("POST", url, strings.NewReader(data))
 	assert.NoError(t, err)
@@ -43,7 +43,7 @@ func TestPost1(t *testing.T) {
 
 func TestPost2(t *testing.T) {
 	url := "http://localhost:8883/api/v1/test-dubbo/UserService/com.dubbogo.pixiu.UserService?group=test&version=1.0.0&method=UpdateUserByName"
-	data := "{\"types\":[\"string\",\"body\"],\"values\":[\"tc\",{\"id\":\"0001\",\"code\":1,\"name\":\"tc\",\"age\":15}]}"
+	data := "{\"types\":\"string,body\",\"values\":[\"tc\",{\"id\":\"0001\",\"code\":1,\"name\":\"tc\",\"age\":15}]}"
 	client := &http.Client{Timeout: 5 * time.Second}
 	req, err := http.NewRequest("POST", url, strings.NewReader(data))
 	assert.NoError(t, err)
@@ -56,7 +56,7 @@ func TestPost2(t *testing.T) {
 
 func TestPost3(t *testing.T) {
 	url := "http://localhost:8883/api/v1/test-dubbo/UserService/com.dubbogo.pixiu.UserService?group=test&version=1.0.0&method=GetUserByCode"
-	data := "{\"types\":[\"int\"],\"values\":1}"
+	data := "{\"types\":\"int\",\"values\":1}"
 	client := &http.Client{Timeout: 5 * time.Second}
 	req, err := http.NewRequest("POST", url, strings.NewReader(data))
 	assert.NoError(t, err)
