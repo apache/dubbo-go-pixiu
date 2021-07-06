e8c840412180671c3c297b0780db2e0fc50240ae
tydhot <583125614@qq.com>
2021-07-06 14:41:21 +0800

    add comment
    Former-commit-id: 708f50300e7480853cada0e2345fa87274437e0b

--- pkg/model/metric.go
+++ pkg/model/metric.go
@@ -21,6 +21,7 @@ import (
 	"time"
 )
 
+// Metirc config fot otel metric.
 type Metric struct {
 	Enable   bool          `yaml:"enable" json:"enable"`
 	Interval time.Duration `yaml:"interval" json:"interval"`
