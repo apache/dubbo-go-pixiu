70b86b0faf99c12aa25f0da44fca7d86aa8cec2a
alchemy-lee <2769566951@qq.com>
2021-07-19 15:38:56 +0800

    add license header

--- pkg/registry/base/baseregistry_test.go
+++ pkg/registry/base/baseregistry_test.go
@@ -1,18 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package baseregistry
 
-import(
-	"testing"
+import (
 	"math/rand"
+	"testing"
 	"time"
 )
 
 import (
-	"github.com/dubbogo/dubbo-go-pixiu-filter/pkg/router"
 	"github.com/apache/dubbo-go/common"
+	"github.com/dubbogo/dubbo-go-pixiu-filter/pkg/router"
 	"github.com/stretchr/testify/assert"
 )
 
-type mockRegFacade struct {}
+type mockRegFacade struct{}
 
 func (m *mockRegFacade) LoadInterfaces() ([]router.API, []error) {
 	return nil, nil
@@ -27,13 +44,13 @@ func (m *mockRegFacade) DoUnsubscribe(*common.URL) error {
 	return nil
 }
 
-func CreateMockAPI(urlPattern string) router.API{
+func CreateMockAPI(urlPattern string) router.API {
 	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	randStringRunes := func(n int) string {
 		rand.Seed(time.Now().UnixNano())
 		b := make([]rune, n)
 		for i := range b {
-				b[i] = letterRunes[rand.Intn(len(letterRunes))]
+			b[i] = letterRunes[rand.Intn(len(letterRunes))]
 		}
 		return string(b)
 	}
@@ -47,7 +64,7 @@ func CreateMockAPI(urlPattern string) router.API{
 
 func TestDeduplicate(t *testing.T) {
 	apis := []router.API{}
-	for i := 0; i<10; i++ {
+	for i := 0; i < 10; i++ {
 		apis = append(apis, CreateMockAPI(""))
 	}
 	var facade *mockRegFacade = &mockRegFacade{}
