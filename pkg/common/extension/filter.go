5abcdd89bee35f116b7305794b4756cf63ade968
mark4z <anymre@outlook.com>
2021-07-21 22:13:52 +0800

    filter manager
    Former-commit-id: be5bfae30e43557f9d582eca84f6d33091a3d909

--- pkg/common/extension/filter.go
+++ pkg/common/extension/filter.go
@@ -18,22 +18,71 @@
 package extension
 
 import (
+	"github.com/apache/dubbo-go-pixiu/pkg/logger"
+	"github.com/dubbogo/dubbo-go-pixiu-filter/pkg/api/config"
 	"github.com/dubbogo/dubbo-go-pixiu-filter/pkg/context"
+	"github.com/dubbogo/dubbo-go-pixiu-filter/pkg/filter"
+	"github.com/ghodss/yaml"
+	"github.com/pkg/errors"
 )
 
-var filterFuncCacheMap = make(map[string]func(ctx context.Context), 8)
+var filterFactories = make(map[string]filterFactoryCreator, 8)
 
-// SetFilterFunc will store the @filter and @name
-func SetFilterFunc(name string, filter context.FilterFunc) {
-	filterFuncCacheMap[name] = filter
+type FilterManager struct {
+	filters []context.Filter
 }
 
-// GetMustFilterFunc will return the pixiu.FilterFunc
-// if not found, it will panic
-func GetMustFilterFunc(name string) context.FilterFunc {
-	if filter, ok := filterFuncCacheMap[name]; ok {
-		return filter
-	}
+type filterFactoryCreator func() filter.Factory
 
-	panic("filter func for " + name + " is not existing!")
+// RegisterFilterFactory will store the @filter factory and @name
+func RegisterFilterFactory(name string, filter filterFactoryCreator) {
+	filterFactories[name] = filter
+}
+
+func (fm *FilterManager) GetFilters() []context.Filter {
+	return fm.filters
+}
+
+func (fm *FilterManager) Init(filters []config.Filter) {
+	tmp := make([]context.Filter, len(filters))
+	for _, f := range filters {
+		apply, err := fm.GetFilter(f.Name, f.Config)
+		if err != nil {
+			logger.Errorf("apply [%s] init fail, %s", err)
+		}
+		tmp = append(tmp, apply)
+	}
+	//TODO lock and hotswap
+	fm.filters = tmp
+}
+
+func (fm FilterManager) GetFilter(name string, conf interface{}) (context.Filter, error) {
+	creator, ok := filterFactories[name]
+	if !ok {
+		return nil, errors.New("filter not found")
+	}
+	factory := creator()
+	factoryConf := factory.Config()
+	if err := parseConfig(factoryConf, conf); err != nil {
+		return nil, errors.Wrap(err, "config error")
+	}
+	apply, err := factory.Apply()
+	if err != nil {
+		return nil, errors.Wrap(err, "create fail")
+	}
+	return apply, nil
+}
+
+func parseConfig(factoryConfStruct interface{}, conf interface{}) error {
+	// conf will be map, convert to yaml
+	yamlBytes, err := yaml.Marshal(conf)
+	if err != nil {
+		return err
+	}
+	// Unmarshal yamlStr to factoryConf
+	err = yaml.Unmarshal(yamlBytes, factoryConfStruct)
+	if err != nil {
+		return err
+	}
+	return nil
 }
