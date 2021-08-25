629ea8a893c53a524829817170755f0fd100581c
Randy <ztelur@gmail.com>
2021-08-25 19:26:31 +0800

    add unit test
    Former-commit-id: 8a82d13784517051fb437a8166dc20fd6a9358b6

---
+++ pkg/server/adapter_manager_test.go
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package server
+
+import (
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+import (
+	"github.com/apache/dubbo-go-pixiu/pkg/common/extension/adapter"
+	"github.com/apache/dubbo-go-pixiu/pkg/model"
+)
+
+type (
+	DemoAdapterPlugin struct{}
+
+	DemoAdapter struct {
+		cfg *Config
+	}
+
+	Config struct {
+		Name string `yaml:"name" json:"name,omitempty"`
+	}
+)
+
+func (d *DemoAdapterPlugin) Kind() string {
+	return "test"
+}
+
+func (p *DemoAdapterPlugin) CreateAdapter(config interface{}, bs *model.Bootstrap) (adapter.Adapter, error) {
+	return &DemoAdapter{cfg: &Config{}}, nil
+}
+
+func (a *DemoAdapter) Start() {
+
+}
+
+func (a *DemoAdapter) Stop() {
+
+}
+
+func TestAdapterManager(t *testing.T) {
+	adapter.RegisterAdapterPlugin(&DemoAdapterPlugin{})
+
+	bs := &model.Bootstrap{
+		StaticResources: model.StaticResources{
+			Adapters: []*model.Adapter{
+				&model.Adapter{
+					Name:   "test",
+					Config: "",
+				},
+			},
+		},
+	}
+
+	am := CreateDefaultAdapterManager(nil, bs)
+
+	assert.Equal(t, len(am.adapters), 1)
+
+	am.Start()
+
+	am.Stop()
+
+}
