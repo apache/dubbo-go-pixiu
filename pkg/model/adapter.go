e3f36110aaf6eab9f1635ba36c8cfa44865b0fc0
Randy <ztelur@gmail.com>
2021-08-25 20:11:43 +0800

    add unit test
    Former-commit-id: a630ba18110888d8c856595aa2d45d90f440176f [formerly d7b349e7f8ef8a27b64d6993436c5f59b14ba9dd]
    Former-commit-id: 30337142e0fd527264d063017df20be29cf6d431

--- pkg/model/adapter.go
+++ pkg/model/adapter.go
@@ -19,6 +19,6 @@ package model
 
 // Adapter the adapter plugin for manage cluster or router
 type Adapter struct {
-	Name   string      `yaml:"name" json:"name"`                           // Name the adapter unique name
-	Config interface{} `yaml:"config" json:"config" mapstructure:"config"` // Config adapter config
+	Name   string                 `yaml:"name" json:"name"`                           // Name the adapter unique name
+	Config map[string]interface{} `yaml:"config" json:"config" mapstructure:"config"` // Config adapter config
 }
