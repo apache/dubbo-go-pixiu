cbaec22eb6ec4b7fe856fcc03764aa0147065b84
phil.yue <phil.yue@foxmail.com>
2021-09-12 18:54:29 +0800

    add eureka
    Former-commit-id: 254f3ae0809f122dacb5a3126c0a1deb0f785733

--- pkg/adapter/springcloud/sc_manager.go
+++ pkg/adapter/springcloud/sc_manager.go
@@ -1,14 +1,31 @@
 package springcloud
 
 import (
+	"github.com/apache/dubbo-go-pixiu/pkg/adapter/springcloud/discovery"
+	"github.com/apache/dubbo-go-pixiu/pkg/logger"
 	"github.com/apache/dubbo-go-pixiu/pkg/model"
 )
+
+// SpringCloudConfig SpringCloud that life cycle configuration
+type SpringCloudConfig struct {
+
+	// runtime context : config properties
+	boot *model.Bootstrap
+
+	discoveryClient			discovery.Client
+
+
+}
 
 // SpringCloud runtime in Pixiu manager
 type scManager struct {
 
 	// runtime context : config properties
-	boot *model.Bootstrap
+	//boot *model.Bootstrap
+
+	scConfig *SpringCloudConfig
+
+	Start func() *scManager
 
 	// init all
 	initAll func()
@@ -24,6 +41,8 @@ type scManager struct {
 
 	// hook
 	postConstruct func()
+
+	Stop func()
 }
 
 // load remote registry : nacos, consul...
@@ -32,6 +51,7 @@ func loadRouterByRemoteConfig() {
 }
 
 func loadRouterByLocalConfig() {
+
 
 }
 
@@ -44,6 +64,8 @@ const (
 	Stopped = "Stopped"
 )
 
+var manager *scManager
+
 var SCX = &sc{
 	status: Initial,
 }
@@ -52,16 +74,41 @@ type sc struct {
 	status string `json:"status"`
 }
 
-func SpringCloudManager(boot *model.Bootstrap) *scManager {
-	
-	return &scManager{
-		boot:                      boot,
+func NewSpringCloudManager(config *SpringCloudConfig) *scManager {
+
+	manager = &scManager{
+		scConfig: config,
+		Start: func() *scManager {
+			manager.initAll()
+			return manager
+		},
 		initAll: func() {
+
+			// 1. 初始化 Register Client ：nacos，eureka，consul...
+			config.discoveryClient, _ = discovery.NewEurekaClient("configs/eureka.json")
+
+			// 1.1 开启定时自动刷新本地服务实例机制
+
+			err := config.discoveryClient.StartPeriodicalRefresh()
+			if err != nil {
+				logger.Errorf("start discovery periodical refresh fail!", err)
+			}
+			// 1.2 注册自己，如果需要的话
+			err = config.discoveryClient.Register()
+			if err != nil {
+				logger.Errorf("register self fail!", err)
+			}
+			// 2.
+
+			//
 			loadRouterByLocalConfig()
 			loadRouterByRemoteConfig()
 		},
 		registryAutoConfiguration: func() {
 
+			//bootstrap := manager.boot
+			//resources := bootstrap.DynamicResources
+			//resources
 		},
 		initRouter: func() {
 
@@ -73,6 +120,10 @@ func SpringCloudManager(boot *model.Bootstrap) *scManager {
 
 		},
 	}
+
+	// manager.Start()
+
+	return manager
 }
 
 //type registry {
