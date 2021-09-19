f0dfa8158e45be7ad8de4dbfae8e42ab0ea08ac9
randy <ztelur@gmail.com>
2021-09-19 22:39:04 +0800

    cloud firstFetch and backgroupSync

--- pkg/adapter/springcloud/servicediscovery/nacos/nacos.go
+++ pkg/adapter/springcloud/servicediscovery/nacos/nacos.go
@@ -4,40 +4,51 @@ import (
 	"github.com/apache/dubbo-go-pixiu/pkg/adapter/springcloud/servicediscovery"
 	"github.com/apache/dubbo-go-pixiu/pkg/model"
 	"github.com/apache/dubbo-go-pixiu/pkg/remote/nacos"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 )
 
 type nacosServiceDiscovery struct {
-	group      string
-	descriptor string
-	client     *nacos.NacosClient
-
-	registryInstances []servicediscovery.InstanceInfo
+	descriptor        string
+	client            *nacos.NacosClient
+	config            *model.RemoteConfig
+	registryInstances []servicediscovery.ServiceInstances
 }
 
-func (n nacosServiceDiscovery) QueryServices() ([]*servicediscovery.InstanceInfo, error) {
+func (n *nacosServiceDiscovery) AddListener(s string) {
 	panic("implement me")
 }
 
-func (n nacosServiceDiscovery) Register() error {
+func (n *nacosServiceDiscovery) Stop() error {
 	panic("implement me")
 }
 
-func (n nacosServiceDiscovery) UnRegister() error {
+func (n *nacosServiceDiscovery) QueryServices() ([]*servicediscovery.ServiceInstances, error) {
+	services, err := n.client.GetAllServicesInfo(vo.GetAllServiceInfoParam{
+		GroupName: n.group,
+	})
 	panic("implement me")
 }
 
-func (n nacosServiceDiscovery) Get(s string) []*servicediscovery.InstanceInfo {
+func (n *nacosServiceDiscovery) Register() error {
 	panic("implement me")
 }
 
-func (n nacosServiceDiscovery) StartPeriodicalRefresh() error {
+func (n *nacosServiceDiscovery) UnRegister() error {
 	panic("implement me")
 }
 
-func NewNacosServiceDiscovery(config model.RemoteConfig) (servicediscovery.ServiceDiscovery, error) {
+func (n *nacosServiceDiscovery) Get(s string) []*servicediscovery.ServiceInstances {
+	panic("implement me")
+}
+
+func (n *nacosServiceDiscovery) StartPeriodicalRefresh() error {
+	panic("implement me")
+}
+
+func NewNacosServiceDiscovery(config *model.RemoteConfig) (servicediscovery.ServiceDiscovery, error) {
 	client, err := nacos.NewNacosClient(config)
 	if err != nil {
 		return nil, err
 	}
-	return nacosServiceDiscovery{client: client}, nil
+	return &nacosServiceDiscovery{client: client, config: config}, nil
 }
