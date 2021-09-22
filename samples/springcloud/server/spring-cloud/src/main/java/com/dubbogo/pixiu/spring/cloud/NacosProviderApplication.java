cfdd9e2756405a91b9b31f44676d5792b5797cdc
Randy <ztelur@gmail.com>
2021-09-22 18:44:28 +0800

    remove change

--- samples/springcloud/server/spring-cloud/src/main/java/com/dubbogo/pixiu/spring/cloud/NacosProviderApplication.java
+++ samples/springcloud/server/spring-cloud/src/main/java/com/dubbogo/pixiu/spring/cloud/NacosProviderApplication.java
@@ -1,34 +1,29 @@
-package com.alibaba.cloud.examples;
+package com.dubbogo.pixiu.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author xiaojing
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProviderApplication {
+public class NacosProviderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProviderApplication.class, args);
+		SpringApplication.run(NacosProviderApplication.class, args);
 	}
 
 	@RestController
 	class EchoController {
-
-		@GetMapping("/echo/{string}")
+		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
 		public String echo(@PathVariable String string) {
-			return "hello Nacos Discovery " + string;
+			return "Hello Nacos Discovery " + string;
 		}
-
-		@GetMapping("/divide")
-		public String divide(@RequestParam Integer a, @RequestParam Integer b) {
-			return String.valueOf(a / b);
-		}
-
 	}
-
 }
