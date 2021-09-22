d4b3ab4c9ba3ce7fc0fbc112bca813c8386878c2
Randy <ztelur@gmail.com>
2021-09-22 18:01:42 +0800

    first version without watch
    Former-commit-id: f7d80524a78acb4542c05449a59a2fb2444ea39b [formerly a93f142588b4e6f8ba372613380f0eb6370ab7ec]
    Former-commit-id: 2e6c710c3e224f6aa3ea403cdca98ec5d8edc2ac

--- pkg/remote/nacos/client.go
+++ pkg/remote/nacos/client.go
@@ -1,21 +1,37 @@
 package nacos
 
 import (
-	"github.com/apache/dubbo-go-pixiu/pkg/model"
-	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/clients/naming_client"
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	perrors "github.com/pkg/errors"
 	"net"
 	"strconv"
 	"strings"
+)
+
+import (
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	model2 "github.com/nacos-group/nacos-sdk-go/model"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	perrors "github.com/pkg/errors"
+)
+
+import (
+	"github.com/apache/dubbo-go-pixiu/pkg/model"
 )
 
 type NacosClient struct {
 	namingClient naming_client.INamingClient
 }
 
-func NewNacosClient(config model.RemoteConfig) (*NacosClient, error) {
+func (client *NacosClient) GetAllServicesInfo(param vo.GetAllServiceInfoParam) (model2.ServiceList, error) {
+	return client.namingClient.GetAllServicesInfo(param)
+}
+
+func (client *NacosClient) SelectInstances(param vo.SelectInstancesParam) ([]model2.Instance, error) {
+	return client.namingClient.SelectInstances(param)
+}
+
+func NewNacosClient(config *model.RemoteConfig) (*NacosClient, error) {
 	configMap := make(map[string]interface{}, 2)
 	addresses := strings.Split(config.Address, ",")
 	serverConfigs := make([]constant.ServerConfig, 0, len(addresses))
