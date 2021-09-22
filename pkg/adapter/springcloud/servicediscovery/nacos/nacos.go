f7d80524a78acb4542c05449a59a2fb2444ea39b
Randy <ztelur@gmail.com>
2021-09-22 18:01:42 +0800

    first version without watch
    Former-commit-id: a93f142588b4e6f8ba372613380f0eb6370ab7ec

--- pkg/adapter/springcloud/servicediscovery/nacos/nacos.go
+++ pkg/adapter/springcloud/servicediscovery/nacos/nacos.go
@@ -1,7 +1,9 @@
 package nacos
 
 import (
+	"fmt"
 	"github.com/apache/dubbo-go-pixiu/pkg/adapter/springcloud/servicediscovery"
+	"github.com/apache/dubbo-go-pixiu/pkg/logger"
 	"github.com/apache/dubbo-go-pixiu/pkg/model"
 	"github.com/apache/dubbo-go-pixiu/pkg/remote/nacos"
 	"github.com/nacos-group/nacos-sdk-go/vo"
@@ -11,7 +13,7 @@ type nacosServiceDiscovery struct {
 	descriptor        string
 	client            *nacos.NacosClient
 	config            *model.RemoteConfig
-	registryInstances []servicediscovery.ServiceInstances
+	registryInstances []servicediscovery.ServiceInstance
 }
 
 func (n *nacosServiceDiscovery) AddListener(s string) {
@@ -22,11 +24,51 @@ func (n *nacosServiceDiscovery) Stop() error {
 	panic("implement me")
 }
 
-func (n *nacosServiceDiscovery) QueryServices() ([]*servicediscovery.ServiceInstances, error) {
+func (n *nacosServiceDiscovery) QueryServices() ([]servicediscovery.ServiceInstance, error) {
 	services, err := n.client.GetAllServicesInfo(vo.GetAllServiceInfoParam{
-		GroupName: n.group,
+		GroupName: n.config.Group,
+		PageSize:  10,
 	})
-	panic("implement me")
+
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]servicediscovery.ServiceInstance, 0, len(services.Doms))
+
+	// need get all service instance api
+	for _, serviceName := range services.Doms {
+
+		instances, err := n.client.SelectInstances(vo.SelectInstancesParam{
+			ServiceName: serviceName,
+			Clusters:    []string{"DEFAULT"},
+			HealthyOnly: true,
+		})
+
+		if err != nil {
+			logger.Warnf("QueryServices SelectInstances {key:%s} = error{%s}", serviceName, err)
+			continue
+		}
+
+		for _, instance := range instances {
+			addr := instance.Ip + ":" + fmt.Sprint(instance.Port)
+			si := servicediscovery.ServiceInstance{
+				// nacos sdk return empty instanceId, so use addr
+				//ID: instance.InstanceId,
+				ID:          addr,
+				ServiceName: serviceName,
+				Host:        instance.Ip,
+				Port:        int(instance.Port),
+				// SelectInstances default return all health instance, not unhealthy
+				Healthy:     instance.Healthy,
+				Enable:      instance.Enable,
+				CLusterName: instance.ClusterName,
+				Metadata:    instance.Metadata,
+			}
+			res = append(res, si)
+		}
+	}
+	return res, nil
 }
 
 func (n *nacosServiceDiscovery) Register() error {
@@ -37,7 +79,7 @@ func (n *nacosServiceDiscovery) UnRegister() error {
 	panic("implement me")
 }
 
-func (n *nacosServiceDiscovery) Get(s string) []*servicediscovery.ServiceInstances {
+func (n *nacosServiceDiscovery) Get(s string) []*servicediscovery.ServiceInstance {
 	panic("implement me")
 }
 
