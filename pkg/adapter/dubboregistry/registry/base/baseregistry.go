463730b555b3c35457edaccddca44fc33e83b1dc
Randy <ztelur@gmail.com>
2021-09-27 18:50:13 +0800

    update
    Former-commit-id: 0be5685a21caf294bc67c48cf26cca47f9c6e9ed [formerly 3957898697a0df53f70380a0a338806ae473240e]
    Former-commit-id: bf99a54dae75a47605f3b3d7053337778c0fc04e

--- pkg/adapter/dubboregistry/registry/base/baseregistry.go
+++ pkg/adapter/dubboregistry/registry/base/baseregistry.go
@@ -1,9 +1,9 @@
 package baseregistry
 
 import (
+	"github.com/apache/dubbo-go-pixiu/pkg/adapter/dubboregistry/common"
 	"github.com/apache/dubbo-go-pixiu/pkg/adapter/dubboregistry/registry"
 	"sync"
-	//"github.com/dubbogo/dubbo-go-pixiu-filter/pkg/router"
 )
 
 type FacadeRegistry interface {
@@ -51,23 +51,19 @@ func (s *SvcListeners) GetAllListener() map[string]registry.Listener {
 }
 
 type BaseRegistry struct {
-	AdapterID      string
-	svcListeners   *SvcListeners
-	facadeRegistry FacadeRegistry
+	svcListeners    *SvcListeners
+	facadeRegistry  FacadeRegistry
+	AdapterListener common.RegistryEventListener
 }
 
-func NewBaseRegistry(facade FacadeRegistry) *BaseRegistry {
+func NewBaseRegistry(facade FacadeRegistry, adapterListener common.RegistryEventListener) *BaseRegistry {
 	return &BaseRegistry{
 		facadeRegistry: facade,
 		svcListeners: &SvcListeners{
 			listeners: make(map[string]registry.Listener),
 		},
+		AdapterListener: adapterListener,
 	}
-}
-
-// SetAdapterID
-func (r *BaseRegistry) SetAdapterID(id string) {
-	r.AdapterID = id
 }
 
 // GetSvcListener returns existing listener or nil
