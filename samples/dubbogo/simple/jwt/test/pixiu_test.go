756ab8776bcea6e445421367b7ed565e546c0032
baerwang <1780285721@qq.com>
2021-11-26 11:18:18 +0800

    feat:springcloud integration auth filter

--- samples/dubbogo/simple/jwt/test/pixiu_test.go
+++ samples/dubbogo/simple/jwt/test/pixiu_test.go
@@ -58,6 +58,13 @@ func TestRemote(t *testing.T) {
 	assert.True(t, strings.Contains(s, "prefix"))
 }
 
+func TestSpringCloud(t *testing.T) {
+	s := verify(t, "http://localhost:8888/user-service/echo/Pixiu", http.StatusOK)
+	assert.True(t, strings.Contains(s, "User"))
+	s = verify(t, "http://localhost:8888/auth-service/echo/Pixiu", http.StatusOK)
+	assert.True(t, strings.Contains(s, "Auth"))
+}
+
 func verify(t *testing.T, url string, status int) string {
 	var token = "eyJraWQiOiJlZThkNjI2ZCIsInR5cCI6IkpXVCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJXZWlkb25nIiwiYXVkIjoiVGFzaHVhbiIsImlzcyI6Imp3a3Mtc2VydmljZS5hcHBzcG90LmNvbSIsImlhdCI6MTYzMTM2OTk1NSwianRpIjoiNDY2M2E5MTAtZWU2MC00NzcwLTgxNjktY2I3NDdiMDljZjU0In0.LwD65d5h6U_2Xco81EClMa_1WIW4xXZl8o4b7WzY_7OgPD2tNlByxvGDzP7bKYA9Gj--1mi4Q4li4CAnKJkaHRYB17baC0H5P9lKMPuA6AnChTzLafY6yf-YadA7DmakCtIl7FNcFQQL2DXmh6gS9J6TluFoCIXj83MqETbDWpL28o3XAD_05UP8VLQzH2XzyqWKi97mOuvz-GsDp9mhBYQUgN3csNXt2v2l-bUPWe19SftNej0cxddyGu06tXUtaS6K0oe0TTbaqc3hmfEiu5G0J8U6ztTUMwXkBvaknE640NPgMQJqBaey0E4u0txYgyvMvvxfwtcOrDRYqYPBnA"
 	client := &http.Client{Timeout: 5 * time.Second}
