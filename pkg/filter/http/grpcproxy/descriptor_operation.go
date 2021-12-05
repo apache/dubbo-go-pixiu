bb0cffc8716bd26391ca63105a82afdefc8f794d
PhilYue <phil.yue@foxmail.com>
2021-12-05 21:19:52 +0800

    refactor: sample add reflection

--- pkg/filter/http/grpcproxy/descriptor_operation.go
+++ pkg/filter/http/grpcproxy/descriptor_operation.go
@@ -19,47 +19,11 @@ package grpcproxy
 
 import (
 	"fmt"
-	"sync"
 )
 
 import (
 	"github.com/jhump/protoreflect/desc"
-	"github.com/jhump/protoreflect/desc/protoparse"
-	"github.com/jhump/protoreflect/dynamic"
 )
-
-type fileSource struct {
-	files  map[string]*desc.FileDescriptor
-	er     *dynamic.ExtensionRegistry
-	erInit sync.Once
-}
-
-// initFromFileDescriptor
-// protoparse.ResolveFilenames(importPaths, fileNames...)
-// rel: https://pkg.go.dev/github.com/jhump/protoreflect/desc/protoparse?utm_source=godoc#ResolveFilenames
-func (f *Filter) initFromFileDescriptor(importPaths []string, fileNames ...string) error {
-	fileNames, err := protoparse.ResolveFilenames(importPaths, fileNames...)
-	if err != nil {
-		return err
-	}
-	p := protoparse.Parser{
-		ImportPaths:           importPaths,
-		InferImportPaths:      len(importPaths) == 0,
-		IncludeSourceCodeInfo: true,
-	}
-	fds, err := p.ParseFiles(fileNames...)
-	if err != nil {
-		return fmt.Errorf("could not parse given files: %v", err)
-	}
-
-	fsrc.files = make(map[string]*desc.FileDescriptor)
-	for _, fd := range fds {
-		name := fd.GetName()
-		fsrc.files[name] = fd
-	}
-
-	return nil
-}
 
 func DescriptorSourceFromFileDescriptors(files ...*desc.FileDescriptor) (*fileSource, error) {
 	fds := map[string]*desc.FileDescriptor{}
@@ -90,20 +54,6 @@ func addFile(fd *desc.FileDescriptor, fds map[string]*desc.FileDescriptor) error
 	return nil
 }
 
-func (fs *fileSource) ListServices() ([]string, error) {
-	set := map[string]bool{}
-	for _, fd := range fs.files {
-		for _, svc := range fd.GetServices() {
-			set[svc.GetFullyQualifiedName()] = true
-		}
-	}
-	sl := make([]string, 0, len(set))
-	for svc := range set {
-		sl = append(sl, svc)
-	}
-	return sl, nil
-}
-
 func (fs *fileSource) GetAllFiles() ([]*desc.FileDescriptor, error) {
 	files := make([]*desc.FileDescriptor, len(fs.files))
 	i := 0
@@ -112,23 +62,4 @@ func (fs *fileSource) GetAllFiles() ([]*desc.FileDescriptor, error) {
 		i++
 	}
 	return files, nil
-}
-
-func (fs *fileSource) FindSymbol(fullyQualifiedName string) (desc.Descriptor, error) {
-	for _, fd := range fs.files {
-		if dsc := fd.FindSymbol(fullyQualifiedName); dsc != nil {
-			return dsc, nil
-		}
-	}
-	return nil, fmt.Errorf("could not found symbol %v", fullyQualifiedName)
-}
-
-func (fs *fileSource) AllExtensionsForType(typeName string) ([]*desc.FieldDescriptor, error) {
-	fs.erInit.Do(func() {
-		fs.er = &dynamic.ExtensionRegistry{}
-		for _, fd := range fs.files {
-			fs.er.AddExtensionsFromFile(fd)
-		}
-	})
-	return fs.er.AllExtensionsForType(typeName), nil
 }
