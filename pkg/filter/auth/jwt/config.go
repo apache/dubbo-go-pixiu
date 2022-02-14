9f39649070952c619e914c9ad332c3d2a2231597
baerwang <1780285721@qq.com>
2022-02-14 22:29:45 +0800

    style:jwt field meaning

--- pkg/filter/auth/jwt/config.go
+++ pkg/filter/auth/jwt/config.go
@@ -24,43 +24,44 @@ import (
 type (
 	// FromHeaders Get the token from a field in the header，default Authorization: Bearer <token>
 	FromHeaders struct {
-		Name        string `default:"Authorization" yaml:"name" json:"name" mapstructure:"name"`
-		ValuePrefix string `default:"Bearer " yaml:"value_prefix" json:"value_prefix" mapstructure:"value_prefix"`
+		Name        string `default:"Authorization" yaml:"name" json:"name" mapstructure:"name"`                   // header key
+		ValuePrefix string `default:"Bearer " yaml:"value_prefix" json:"value_prefix" mapstructure:"value_prefix"` // header value
 	}
 
+	// Rules router match
 	Rules struct {
-		Match    Match    `yaml:"match" json:"match" mapstructure:"match"`
-		Requires Requires `yaml:"requires" json:"requires" mapstructure:"requires"`
+		Match    Match    `yaml:"match" json:"match" mapstructure:"match"`          // router
+		Requires Requires `yaml:"requires" json:"requires" mapstructure:"requires"` // router requires jwks check
 	}
 
 	Match struct {
-		Prefix string `yaml:"prefix" json:"prefix" mapstructure:"prefix"`
+		Prefix string `yaml:"prefix" json:"prefix" mapstructure:"prefix"` // url
 	}
 
 	Requires struct {
-		RequiresAny Requirement   `yaml:"requires_any" json:"requires_any" mapstructure:"requires_any"`
-		RequiresAll []Requirement `yaml:"requires_all" json:"requires_all" mapstructure:"requires_all"`
+		RequiresAny Requirement   `yaml:"requires_any" json:"requires_any" mapstructure:"requires_any"` // single token check
+		RequiresAll []Requirement `yaml:"requires_all" json:"requires_all" mapstructure:"requires_all"` // multiple token check,will only verify one, if the token fails, continue to the next check
 	}
 
 	Requirement struct {
-		ProviderName string `yaml:"provider_name" json:"provider_name" mapstructure:"provider_name"`
+		ProviderName string `yaml:"provider_name" json:"provider_name" mapstructure:"provider_name"` // jwks providers name
 	}
 
 	Providers struct {
-		Name                 string      `yaml:"name" json:"name" mapstructure:"name"`
-		ForwardPayloadHeader string      `yaml:"forward_payload_header" json:"forward_payload_header" mapstructure:"forward_payload_header"`
-		FromHeaders          FromHeaders `yaml:"from_headers" json:"from_headers" mapstructure:"from_headers"`
-		Issuer               string      `yaml:"issuer" json:"issuer" mapstructure:"issuer"`
-		Local                *Local      `yaml:"local_jwks" json:"local_jwks" mapstructure:"local_jwks"`
-		Remote               *Remote     `yaml:"remote_jwks" json:"remote_jwks" mapstructure:"remote_jwks"`
+		Name                 string      `yaml:"name" json:"name" mapstructure:"name"`                                                       // jwt name
+		ForwardPayloadHeader string      `yaml:"forward_payload_header" json:"forward_payload_header" mapstructure:"forward_payload_header"` // header add issuer
+		FromHeaders          FromHeaders `yaml:"from_headers" json:"from_headers" mapstructure:"from_headers"`                               // from header get token
+		Issuer               string      `yaml:"issuer" json:"issuer" mapstructure:"issuer"`                                                 // jwt issuer
+		Local                *Local      `yaml:"local_jwks" json:"local_jwks" mapstructure:"local_jwks"`                                     // local jwks
+		Remote               *Remote     `yaml:"remote_jwks" json:"remote_jwks" mapstructure:"remote_jwks"`                                  // remote jwks
 	}
 
 	Local struct {
-		InlineString string `yaml:"inline_string" json:"inline_string" mapstructure:"inline_string"`
+		InlineString string `yaml:"inline_string" json:"inline_string" mapstructure:"inline_string"` // local jwks public key
 	}
 
 	Remote struct {
-		HttpURI HttpURI `yaml:"http_uri" json:"http_uri" mapstructure:"http_uri"`
+		HttpURI HttpURI `yaml:"http_uri" json:"http_uri" mapstructure:"http_uri"` // remote jwks public key
 	}
 
 	HttpURI struct {
