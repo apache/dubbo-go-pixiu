325f8c2eca8231868bdb79c19e20905bec711b93
maxing <1501901576@qq.com>
2022-04-03 18:52:49 +0800

    trace code adjust

--- pkg/trace/otlp/otlp.go
+++ pkg/trace/otlp/otlp.go
@@ -1,14 +1,7 @@
 package otlp
 
-import (
-	"context"
-	"github.com/apache/dubbo-go-pixiu/pkg/model"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-)
-
-func NewOTLPExporter(ctx context.Context, cfg *model.TracerConfig) (sdktrace.SpanExporter, error) {
-	client := otlptracehttp.NewClient()
-	return otlptrace.New(ctx, client)
-}
+//
+//func NewOTLPExporter(ctx context.Context, cfg *model.TracerConfig) (sdktrace.SpanExporter, error) {
+//	client := otlptracehttp.NewClient()
+//	return otlptrace.New(ctx, client)
+//}
--- pkg/trace/trace_manager.go
+++ pkg/trace/trace_manager.go
@@ -1,22 +1,16 @@
 package trace
 
 import (
-	"errors"
-	"fmt"
 	"github.com/apache/dubbo-go-pixiu/pkg/model"
-	"github.com/apache/dubbo-go-pixiu/pkg/server"
-	"github.com/apache/dubbo-go-pixiu/pkg/trace/protocol"
-	"strings"
-	"sync/atomic"
 )
 
-type traceGenerator func(protocol.Trace) protocol.Trace
+type traceGenerator func(Trace) Trace
 
-var traceFactory map[protocolName]traceGenerator
+var TraceFactory map[ProtocolName]traceGenerator
 
 func init() {
-	traceFactory = make(map[protocolName]traceGenerator)
-	traceFactory[HTTP] = protocol.NewHTTPTracer
+	TraceFactory = make(map[ProtocolName]traceGenerator)
+	TraceFactory[HTTP] = NewHTTPTracer
 }
 
 type TraceDriverManager struct {
@@ -33,48 +27,11 @@ func CreateDefaultTraceDriverManager(bs *model.Bootstrap) *TraceDriverManager {
 	return manager
 }
 
-func NewTracer(name protocolName) protocol.Trace {
-	driver := server.GetTraceDriverManager().driver
-	holder, ok := driver.holders[name]
-	if !ok {
-		holder = &Holder{
-			Tracers: make(map[string]protocol.Trace),
-		}
-		holder.Id = 0
-		driver.holders[name] = holder
-	}
-	// tarceId的生成，并且在协议接口唯一
-	builder := strings.Builder{}
-	builder.WriteString(string(name))
-	builder.WriteString("-" + string(holder.Id))
-
-	traceId := builder.String()
-	tmp := driver.tp.Tracer(traceId)
-	tracer := &protocol.Tracer{
-		Id: traceId,
-		T:  tmp,
-		H:  holder,
-	}
-
-	holder.Tracers[traceId] = traceFactory[HTTP](tracer)
-
-	atomic.AddUint64(&holder.Id, 1)
-	return holder.Tracers[traceId]
-}
-
-func GetTracer(name protocolName, tracerId string) (protocol.Trace, error) {
-	driver := server.GetTraceDriverManager().driver
-	holder, ok := driver.holders[name]
-	if !ok {
-		return nil, errors.New("can not find any tracer, please call NewTracer first")
-	} else if _, ok = holder.Tracers[tracerId]; !ok {
-		return nil, errors.New(fmt.Sprintf("can not find tracer %s with protocol %s", tracerId, name))
-	} else {
-		return holder.Tracers[tracerId], nil
-	}
+func (manager *TraceDriverManager) GetDriver() *TraceDriver {
+	return manager.driver
 }
 
 func (manager *TraceDriverManager) Close() {
 	driver := manager.driver
-	_ = driver.tp.Shutdown(driver.context)
+	_ = driver.Tp.Shutdown(driver.context)
 }
