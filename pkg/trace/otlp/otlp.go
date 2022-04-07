85c7217adcee78a6a44af724d2735bdd7d7d3063
maxing <1501901576@qq.com>
2022-04-07 19:16:39 +0800

    feature: trace support

--- pkg/trace/otlp/otlp.go
+++
@@ -1,7 +0,0 @@
-package otlp
-
-//
-//func NewOTLPExporter(ctx context.Context, cfg *model.TracerConfig) (sdktrace.SpanExporter, error) {
-//	client := otlptracehttp.NewClient()
-//	return otlptrace.New(ctx, client)
-//}
