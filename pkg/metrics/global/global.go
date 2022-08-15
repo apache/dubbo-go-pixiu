452eebba3a7db7b093409c15c840801dd9b270e3
gongna <2036479155@qq.com>
2022-08-15 16:23:00 +0800

    feat: Pixiu Metrics #433

--- pkg/metrics/global/global.go
+++ pkg/metrics/global/global.go
@@ -17,10 +17,7 @@
 
 package global
 
-//Mainly complete the encapsulation of some public class code and the definition of some global keywords.
-
 const (
-	// Exporter Namespace.
 	Namespace      = "pixiu"
 	DefaultEnabled = true
 )
