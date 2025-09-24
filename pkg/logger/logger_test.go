1d7bc4cafd02facfcc6555cce97f02df6ac81785
Alanxtl <m134679102365478@163.com>
2025-09-24 15:29:48 +0800

    fix sonar

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -54,7 +54,7 @@ func readAll(t *testing.T, path string) string {
 	return string(b)
 }
 
-func TestDisableStacktraceTrue_NoStackEvenOnError(t *testing.T) {
+func TestDisableStacktraceTrueNoStackEvenOnError(t *testing.T) {
 	cfg, out := newDevConfigToFile(t)
 	cfg.DisableStacktrace = true // YAML equals to：disableStacktrace: true
 	InitLogger(cfg)
@@ -79,7 +79,7 @@ func TestDisableStacktraceTrue_NoStackEvenOnError(t *testing.T) {
 	}
 }
 
-func TestSetLoggerLevel_DoesNotRebuildAndTakesEffect(t *testing.T) {
+func TestSetLoggerLevelDoesNotRebuildAndTakesEffect(t *testing.T) {
 	cfg, out := newDevConfigToFile(t)
 	InitLogger(cfg)
 
@@ -111,13 +111,18 @@ func TestSetLoggerLevel_DoesNotRebuildAndTakesEffect(t *testing.T) {
 	}
 }
 
-func TestHotReload_RebuildsAndSwitchesSink(t *testing.T) {
+func TestHotReloadRebuildsAndSwitchesSink(t *testing.T) {
 	// cfg1 -> out1
 	cfg1, out1 := newDevConfigToFile(t)
 	InitLogger(cfg1)
 	l1 := GetLogger().SugaredLogger
 
-	GetLogger().Info("hello-1")
+	const (
+		H1 = "hello-1"
+		H2 = "hello-2"
+	)
+
+	GetLogger().Info(H1)
 	_ = GetLogger().Sync()
 
 	// cfg2 -> out2（new sink）
@@ -130,24 +135,24 @@ func TestHotReload_RebuildsAndSwitchesSink(t *testing.T) {
 		t.Fatalf("HotReload should rebuild logger; got same pointer %p", l1)
 	}
 
-	GetLogger().Info("hello-2")
+	GetLogger().Info(H2)
 	_ = GetLogger().Sync()
 
 	got1 := readAll(t, out1)
 	got2 := readAll(t, out2)
 
-	if !strings.Contains(got1, "hello-1") {
+	if !strings.Contains(got1, H1) {
 		t.Fatalf("out1 should contain hello-1 but not found:\n%s", got1)
 	}
-	if strings.Contains(got1, "hello-2") {
+	if strings.Contains(got1, H2) {
 		t.Fatalf("out1 should NOT contain hello-2 after reload:\n%s", got1)
 	}
-	if !strings.Contains(got2, "hello-2") {
+	if !strings.Contains(got2, H2) {
 		t.Fatalf("out2 should contain hello-2 but not found:\n%s", got2)
 	}
 }
 
-func TestPaddedCallerEncoder_FixedWidthAtLeast30(t *testing.T) {
+func TestPaddedCallerEncoderFixedWidthAtLeast30(t *testing.T) {
 	caller := zapcore.EntryCaller{
 		Defined: true,
 		File:    "a/b/c.go",
@@ -174,37 +179,37 @@ type stringCollector struct {
 // mock zapcore.PrimitiveArrayEncoder
 func (s *stringCollector) AppendString(v string) { s.items = append(s.items, v) }
 
-func (s *stringCollector) AppendBool(bool)                      {}
-func (s *stringCollector) AppendByteString([]byte)              {}
-func (s *stringCollector) AppendComplex128(complex128)          {}
-func (s *stringCollector) AppendComplex64(complex64)            {}
-func (s *stringCollector) AppendDuration(time.Duration)         {}
-func (s *stringCollector) AppendFloat64(float64)                {}
-func (s *stringCollector) AppendFloat32(float32)                {}
-func (s *stringCollector) AppendInt(int)                        {}
-func (s *stringCollector) AppendInt64(int64)                    {}
-func (s *stringCollector) AppendInt32(int32)                    {}
-func (s *stringCollector) AppendInt16(int16)                    {}
-func (s *stringCollector) AppendInt8(int8)                      {}
-func (s *stringCollector) AppendTime(time.Time)                 {}
-func (s *stringCollector) AppendUint(uint)                      {}
-func (s *stringCollector) AppendUint64(uint64)                  {}
-func (s *stringCollector) AppendUint32(uint32)                  {}
-func (s *stringCollector) AppendUint16(uint16)                  {}
-func (s *stringCollector) AppendUint8(uint8)                    {}
-func (s *stringCollector) AppendUintptr(uintptr)                {}
-func (s *stringCollector) AppendReflected(any)                  {}
-func (s *stringCollector) AppendArray(zapcore.ArrayMarshaler)   {}
-func (s *stringCollector) AppendObject(zapcore.ObjectMarshaler) {}
-func (s *stringCollector) AppendBinary([]byte)                  {}
-func (s *stringCollector) AppendComplex(complex128)             {}
-func (s *stringCollector) AppendDurationRef(time.Duration)      {}
-func (s *stringCollector) AppendTimeLayout(time.Time, string)   {}
-func (s *stringCollector) AppendIP(ip any)                      {}
-func (s *stringCollector) AppendIPNet(net any)                  {}
-func (s *stringCollector) AppendMAC(mac any)                    {}
-func (s *stringCollector) AppendHex(any)                        {}
-func (s *stringCollector) AppendFloat(any)                      {}
+func (s *stringCollector) AppendBool(bool)                      { /*mock*/ }
+func (s *stringCollector) AppendByteString([]byte)              { /*mock*/ }
+func (s *stringCollector) AppendComplex128(complex128)          { /*mock*/ }
+func (s *stringCollector) AppendComplex64(complex64)            { /*mock*/ }
+func (s *stringCollector) AppendDuration(time.Duration)         { /*mock*/ }
+func (s *stringCollector) AppendFloat64(float64)                { /*mock*/ }
+func (s *stringCollector) AppendFloat32(float32)                { /*mock*/ }
+func (s *stringCollector) AppendInt(int)                        { /*mock*/ }
+func (s *stringCollector) AppendInt64(int64)                    { /*mock*/ }
+func (s *stringCollector) AppendInt32(int32)                    { /*mock*/ }
+func (s *stringCollector) AppendInt16(int16)                    { /*mock*/ }
+func (s *stringCollector) AppendInt8(int8)                      { /*mock*/ }
+func (s *stringCollector) AppendTime(time.Time)                 { /*mock*/ }
+func (s *stringCollector) AppendUint(uint)                      { /*mock*/ }
+func (s *stringCollector) AppendUint64(uint64)                  { /*mock*/ }
+func (s *stringCollector) AppendUint32(uint32)                  { /*mock*/ }
+func (s *stringCollector) AppendUint16(uint16)                  { /*mock*/ }
+func (s *stringCollector) AppendUint8(uint8)                    { /*mock*/ }
+func (s *stringCollector) AppendUintptr(uintptr)                { /*mock*/ }
+func (s *stringCollector) AppendReflected(any)                  { /*mock*/ }
+func (s *stringCollector) AppendArray(zapcore.ArrayMarshaler)   { /*mock*/ }
+func (s *stringCollector) AppendObject(zapcore.ObjectMarshaler) { /*mock*/ }
+func (s *stringCollector) AppendBinary([]byte)                  { /*mock*/ }
+func (s *stringCollector) AppendComplex(complex128)             { /*mock*/ }
+func (s *stringCollector) AppendDurationRef(time.Duration)      { /*mock*/ }
+func (s *stringCollector) AppendTimeLayout(time.Time, string)   { /*mock*/ }
+func (s *stringCollector) AppendIP(ip any)                      { /*mock*/ }
+func (s *stringCollector) AppendIPNet(net any)                  { /*mock*/ }
+func (s *stringCollector) AppendMAC(mac any)                    { /*mock*/ }
+func (s *stringCollector) AppendHex(any)                        { /*mock*/ }
+func (s *stringCollector) AppendFloat(any)                      { /*mock*/ }
 func (s *stringCollector) Cap() int                             { return 0 }
 func (s *stringCollector) Len() int                             { return len(s.items) }
-func (s *stringCollector) Truncate(int)                         {}
+func (s *stringCollector) Truncate(int)                         { /*mock*/ }
