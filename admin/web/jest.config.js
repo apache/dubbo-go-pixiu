8df16de8a91e60c6f6238e7cf33c8a6b74d71558
0xjah <ahmad.jahaf@proton.me>
2025-12-31 02:38:27 +0300

    Update Jest configuration and dependencies for improved Vue testing

--- admin/web/jest.config.js
+++ admin/web/jest.config.js
@@ -22,7 +22,7 @@ module.exports = {
     'vue'
   ],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue2-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
   },
